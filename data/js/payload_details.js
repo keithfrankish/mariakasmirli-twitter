35e18d9f31853ed85f7d4d5d6545e60c7cad57e9
Keith Frankish <k.frankish@gmail.com>
2017-03-31 04:12:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1045,
-	"created_at": "Mon Mar 27 01:12:47 +0000 2017",
+	"tweets": 1046,
+	"created_at": "Fri Mar 31 01:12:45 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,249 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Thu Mar 30 11:56:13 +0000 2017",
+		"id": 847417176822652900,
+		"id_str": "847417176822652933",
+		"text": "RT @EU_EESC: Countdown to #YEYS2017📢 Only one week left!\n  ☑️33 schools\n  ☑️100 students\n  ☑️Topic: #FutureofEU\n  ☑ https://t.co/bOPFFr9PLt…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YEYS2017",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"text": "FutureofEU",
+					"indices": [
+						100,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EU_EESC",
+					"name": "EESC",
+					"id": 257415817,
+					"id_str": "257415817",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/bOPFFr9PLt",
+					"expanded_url": "http://eesc.europa.eu/yeys2017",
+					"display_url": "eesc.europa.eu/yeys2017",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 425,
+			"friends_count": 353,
+			"listed_count": 25,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 1570,
+			"utc_offset": 10800,
+			"time_zone": "Athens",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1037,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/437877662/x0d3dc0751702c3ddc4d926aba4b26e8.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/437877662/x0d3dc0751702c3ddc4d926aba4b26e8.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/845737136401907714/bnW_nPGo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/845737136401907714/bnW_nPGo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1489584217",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Mar 23 09:38:13 +0000 2017",
+			"id": 844845730015563800,
+			"id_str": "844845730015563778",
+			"text": "Countdown to #YEYS2017📢 Only one week left!\n  ☑️33 schools\n  ☑️100 students\n  ☑️Topic: #FutureofEU\n  ☑… https://t.co/Ovmr6UtVKW",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "YEYS2017",
+						"indices": [
+							13,
+							22
+						]
+					},
+					{
+						"text": "FutureofEU",
+						"indices": [
+							87,
+							98
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Ovmr6UtVKW",
+						"expanded_url": "https://twitter.com/i/web/status/844845730015563778",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							104,
+							127
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 257415817,
+				"id_str": "257415817",
+				"name": "EESC",
+				"screen_name": "EU_EESC",
+				"location": "Brussels",
+				"description": "The European Economic and Social Committee is a consultative body of the EU representing #civilsociety. For plenary content, follow #EESCplenary",
+				"url": "https://t.co/d5Kw5rPtfS",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/d5Kw5rPtfS",
+								"expanded_url": "http://www.eesc.europa.eu",
+								"display_url": "eesc.europa.eu",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 26604,
+				"friends_count": 1075,
+				"listed_count": 922,
+				"created_at": "Fri Feb 25 11:07:32 +0000 2011",
+				"favourites_count": 1416,
+				"utc_offset": 7200,
+				"time_zone": "Brussels",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 8965,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "7FBDD4",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/537624948659859458/t-NdVMLX.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/537624948659859458/t-NdVMLX.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/795917851173515265/VQIrlAuB_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/795917851173515265/VQIrlAuB_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/257415817/1490182327",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Mar 26 14:08:54 +0000 2017",
 		"id": 846001014687776800,
 		"id_str": "846001014687776769",
