c82e0a1434fc5b0de091cd86ef97aec539a0bb0d
Keith Frankish <k.frankish@gmail.com>
2017-10-07 04:12:48 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1143,
-	"created_at": "Sun Oct 01 01:12:47 +0000 2017",
+	"tweets": 1144,
+	"created_at": "Sat Oct 07 01:12:46 +0000 2017",
 	"lang": "en"
 }
---
+++ data/js/tweets/2017_10.js
@@ -0,0 +1,404 @@
+Grailbird.data.tweets_2017_10 = 
+[
+	{
+		"created_at": "Fri Oct 06 14:19:53 +0000 2017",
+		"id": 916307026652942300,
+		"id_str": "916307026652942337",
+		"text": "RT @UB_Psyc: \"Introspection is always retrospection. \" - Jean Paul Satre #QuoteOfTheDay #Buckingham #Psychology https://t.co/39A1Sg3U1y",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "QuoteOfTheDay",
+					"indices": [
+						73,
+						87
+					]
+				},
+				{
+					"text": "Buckingham",
+					"indices": [
+						88,
+						99
+					]
+				},
+				{
+					"text": "Psychology",
+					"indices": [
+						100,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UB_Psyc",
+					"name": "UB Psychology Dept.",
+					"id": 3224768038,
+					"id_str": "3224768038",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 916306050533527600,
+					"id_str": "916306050533527552",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+					"url": "https://t.co/39A1Sg3U1y",
+					"display_url": "pic.twitter.com/39A1Sg3U1y",
+					"expanded_url": "https://twitter.com/UB_Psyc/status/916306053557641220/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1181,
+							"h": 709,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1181,
+							"h": 709,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 916306053557641200,
+					"source_status_id_str": "916306053557641220",
+					"source_user_id": 3224768038,
+					"source_user_id_str": "3224768038"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 916306050533527600,
+					"id_str": "916306050533527552",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+					"url": "https://t.co/39A1Sg3U1y",
+					"display_url": "pic.twitter.com/39A1Sg3U1y",
+					"expanded_url": "https://twitter.com/UB_Psyc/status/916306053557641220/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1181,
+							"h": 709,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1181,
+							"h": 709,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 916306053557641200,
+					"source_status_id_str": "916306053557641220",
+					"source_user_id": 3224768038,
+					"source_user_id_str": "3224768038"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 423,
+			"friends_count": 362,
+			"listed_count": 28,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 1803,
+			"utc_offset": 10800,
+			"time_zone": "Athens",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1129,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/437877662/x0d3dc0751702c3ddc4d926aba4b26e8.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/437877662/x0d3dc0751702c3ddc4d926aba4b26e8.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/845737136401907714/bnW_nPGo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/845737136401907714/bnW_nPGo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1489584217",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Oct 06 14:16:01 +0000 2017",
+			"id": 916306053557641200,
+			"id_str": "916306053557641220",
+			"text": "\"Introspection is always retrospection. \" - Jean Paul Satre #QuoteOfTheDay #Buckingham #Psychology https://t.co/39A1Sg3U1y",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "QuoteOfTheDay",
+						"indices": [
+							60,
+							74
+						]
+					},
+					{
+						"text": "Buckingham",
+						"indices": [
+							75,
+							86
+						]
+					},
+					{
+						"text": "Psychology",
+						"indices": [
+							87,
+							98
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 916306050533527600,
+						"id_str": "916306050533527552",
+						"indices": [
+							99,
+							122
+						],
+						"media_url": "http://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+						"url": "https://t.co/39A1Sg3U1y",
+						"display_url": "pic.twitter.com/39A1Sg3U1y",
+						"expanded_url": "https://twitter.com/UB_Psyc/status/916306053557641220/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 408,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1181,
+								"h": 709,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1181,
+								"h": 709,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 916306050533527600,
+						"id_str": "916306050533527552",
+						"indices": [
+							99,
+							122
+						],
+						"media_url": "http://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DLdff4eXkAASGz0.jpg",
+						"url": "https://t.co/39A1Sg3U1y",
+						"display_url": "pic.twitter.com/39A1Sg3U1y",
+						"expanded_url": "https://twitter.com/UB_Psyc/status/916306053557641220/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 408,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1181,
+								"h": 709,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1181,
+								"h": 709,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://eclincher.com\" rel=\"nofollow\">eClincher</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3224768038,
+				"id_str": "3224768038",
+				"name": "UB Psychology Dept.",
+				"screen_name": "UB_Psyc",
+				"location": "",
+				"description": "Department of Psychology @UniOfBuckingham , offering BPS accredited BSc degrees, and MSc / DPhil research degrees. Unique tutorial-based learning environment.",
+				"url": "https://t.co/mxmorvARmv",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mxmorvARmv",
+								"expanded_url": "http://www.buckingham.ac.uk/psychology",
+								"display_url": "buckingham.ac.uk/psychology",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 281,
+				"friends_count": 90,
+				"listed_count": 48,
+				"created_at": "Thu Apr 30 15:49:15 +0000 2015",
+				"favourites_count": 189,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5945,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/868070405256466432/ouoxm2Vj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/868070405256466432/ouoxm2Vj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3224768038/1495798973",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 11,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
