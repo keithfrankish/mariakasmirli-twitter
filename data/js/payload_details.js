f368b49381e5522e95d56de57612131d55d604ae
Keith Frankish <k.frankish@gmail.com>
2018-11-06 02:11:45 +0200

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1486,
-	"created_at": "Mon Nov 05 01:12:45 +0000 2018",
+	"tweets": 1490,
+	"created_at": "Tue Nov 06 00:11:45 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,42 +1,63 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
-		"created_at": "Tue Sep 25 14:27:35 +0000 2018",
-		"id": 1044594266570403800,
-		"id_str": "1044594266570403840",
-		"full_text": "We’ve worked closely with teachers and experts on this FREE guide to help you unpick some of the key ideas and to experience maths mastery for yourself.",
+		"created_at": "Mon Nov 05 18:48:43 +0000 2018",
+		"id": 1059517884211384300,
+		"id_str": "1059517884211384320",
+		"full_text": "Turning ideas into action – implementation of the revised European key competences for lifelong learning from the Intellectual Property Education perspective. Read the report!  @EU_IPO https://t.co/6nnVKj13OG",
 		"truncated": false,
 		"display_text_range": [
 			0,
-			152
+			208
 		],
 		"entities": {
 			"hashtags": [],
 			"symbols": [],
-			"user_mentions": [],
-			"urls": []
+			"user_mentions": [
+				{
+					"screen_name": "EU_IPO",
+					"name": "EUIPO",
+					"id": 70650192,
+					"id_str": "70650192",
+					"indices": [
+						177,
+						184
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6nnVKj13OG",
+					"expanded_url": "https://kivinen.files.wordpress.com/2018/11/kivinen-best-practice-report.pdf",
+					"display_url": "kivinen.files.wordpress.com/2018/11/kivine…",
+					"indices": [
+						185,
+						208
+					]
+				}
+			]
 		},
-		"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter Ads Composer</a>",
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
 		"in_reply_to_status_id": null,
 		"in_reply_to_status_id_str": null,
 		"in_reply_to_user_id": null,
 		"in_reply_to_user_id_str": null,
 		"in_reply_to_screen_name": null,
 		"user": {
-			"id": 229849002,
-			"id_str": "229849002",
-			"name": "Pearson UK",
-			"screen_name": "Pearson_UK",
-			"location": "Mainly London",
-			"description": "We're the corporate affairs team for Pearson's educational businesses in the UK.",
-			"url": "https://t.co/enORXYDxhW",
+			"id": 77735405,
+			"id_str": "77735405",
+			"name": "Kari Kivinen",
+			"screen_name": "Kari_Kivinen",
+			"location": "Helsinki, Suomi",
+			"description": "Interested in education, motivation, self-regulation,  multilingualism, art, IP, IT, French,  etc.",
+			"url": "https://t.co/sKuluJBIXh",
 			"entities": {
 				"url": {
 					"urls": [
 						{
-							"url": "https://t.co/enORXYDxhW",
-							"expanded_url": "http://pearson.com/uk",
-							"display_url": "pearson.com/uk",
+							"url": "https://t.co/sKuluJBIXh",
+							"expanded_url": "http://kivinen.wordpress.com",
+							"display_url": "kivinen.wordpress.com",
 							"indices": [
 								0,
 								23
@@ -49,124 +70,16 @@ Grailbird.data.tweets_2018_11 =
 				}
 			},
 			"protected": false,
-			"followers_count": 5268,
-			"friends_count": 1045,
-			"listed_count": 86,
-			"created_at": "Thu Dec 23 14:05:23 +0000 2010",
-			"favourites_count": 842,
+			"followers_count": 539,
+			"friends_count": 817,
+			"listed_count": 21,
+			"created_at": "Sun Sep 27 13:29:13 +0000 2009",
+			"favourites_count": 4743,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 3685,
-			"lang": "en",
-			"contributors_enabled": false,
-			"is_translator": false,
-			"is_translation_enabled": false,
-			"profile_background_color": "FBF5EA",
-			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/684784544134901761/OkmnQI_z_normal.png",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/684784544134901761/OkmnQI_z_normal.png",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/229849002/1452100529",
-			"profile_link_color": "364295",
-			"profile_sidebar_border_color": "FFFFFF",
-			"profile_sidebar_fill_color": "DDEEF6",
-			"profile_text_color": "333333",
-			"profile_use_background_image": false,
-			"has_extended_profile": false,
-			"default_profile": false,
-			"default_profile_image": false,
-			"following": false,
-			"follow_request_sent": false,
-			"notifications": false,
-			"translator_type": "none"
-		},
-		"geo": null,
-		"coordinates": null,
-		"place": null,
-		"contributors": null,
-		"is_quote_status": false,
-		"retweet_count": 130,
-		"favorite_count": 279,
-		"favorited": true,
-		"retweeted": true,
-		"scopes": {
-			"followers": false
-		},
-		"lang": "en",
-		"text": "We’ve worked closely with teachers and experts on this FREE guide to help you unpick some of the key ideas and to experience maths mastery for yourself."
-	},
-	{
-		"created_at": "Sat Nov 03 02:05:52 +0000 2018",
-		"id": 1058540733387493400,
-		"id_str": "1058540733387493377",
-		"full_text": "Astronomer Jill Tarter discusses the search for intelligent life. She speculates (&amp; I agree) that a civilization advanced enough to reach us will have figured out how to control its aggression &amp; will have no interest in annihilating us.  https://t.co/5lja9jCzn4",
-		"truncated": false,
-		"display_text_range": [
-			0,
-			269
-		],
-		"entities": {
-			"hashtags": [],
-			"symbols": [],
-			"user_mentions": [],
-			"urls": [
-				{
-					"url": "https://t.co/5lja9jCzn4",
-					"expanded_url": "https://phys.org/news/2018-11-astronomer-jill-tarter-discusses-intelligent.html",
-					"display_url": "phys.org/news/2018-11-a…",
-					"indices": [
-						246,
-						269
-					]
-				}
-			]
-		},
-		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
-		"in_reply_to_status_id": null,
-		"in_reply_to_status_id_str": null,
-		"in_reply_to_user_id": null,
-		"in_reply_to_user_id_str": null,
-		"in_reply_to_screen_name": null,
-		"user": {
-			"id": 107225267,
-			"id_str": "107225267",
-			"name": "Steven Pinker",
-			"screen_name": "sapinker",
-			"location": "Boston, MA",
-			"description": "Cognitive scientist at Harvard.",
-			"url": "http://t.co/5OVstkPquV",
-			"entities": {
-				"url": {
-					"urls": [
-						{
-							"url": "http://t.co/5OVstkPquV",
-							"expanded_url": "http://pinker.wjh.harvard.edu",
-							"display_url": "pinker.wjh.harvard.edu",
-							"indices": [
-								0,
-								22
-							]
-						}
-					]
-				},
-				"description": {
-					"urls": []
-				}
-			},
-			"protected": false,
-			"followers_count": 472951,
-			"friends_count": 43,
-			"listed_count": 8410,
-			"created_at": "Thu Jan 21 22:20:32 +0000 2010",
-			"favourites_count": 5,
-			"utc_offset": null,
-			"time_zone": null,
-			"geo_enabled": true,
-			"verified": true,
-			"statuses_count": 6085,
+			"statuses_count": 3169,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -175,16 +88,16 @@ Grailbird.data.tweets_2018_11 =
 			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
 			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
 			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/953839152511909888/-3EQDjTM_normal.jpg",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/953839152511909888/-3EQDjTM_normal.jpg",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/107225267/1517556838",
-			"profile_link_color": "1DA1F2",
+			"profile_image_url": "http://pbs.twimg.com/profile_images/845656389871652865/Turo5DGt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/845656389871652865/Turo5DGt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/77735405/1496085694",
+			"profile_link_color": "1B95E0",
 			"profile_sidebar_border_color": "C0DEED",
 			"profile_sidebar_fill_color": "DDEEF6",
 			"profile_text_color": "333333",
 			"profile_use_background_image": true,
 			"has_extended_profile": false,
-			"default_profile": true,
+			"default_profile": false,
 			"default_profile_image": false,
 			"following": true,
 			"follow_request_sent": false,
@@ -196,13 +109,13 @@ Grailbird.data.tweets_2018_11 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 309,
-		"favorite_count": 1165,
-		"favorited": true,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Astronomer Jill Tarter discusses the search for intelligent life. She speculates (&amp; I agree) that a civilization advanced enough to reach us will have figured out how to control its aggression &amp; will have no interest in annihilating us.  https://t.co/5lja9jCzn4"
+		"text": "Turning ideas into action – implementation of the revised European key competences for lifelong learning from the Intellectual Property Education perspective. Read the report!  @EU_IPO https://t.co/6nnVKj13OG"
 	},
 	{
 		"created_at": "Tue Sep 25 14:27:35 +0000 2018",
@@ -253,16 +166,16 @@ Grailbird.data.tweets_2018_11 =
 				}
 			},
 			"protected": false,
-			"followers_count": 5268,
-			"friends_count": 1045,
+			"followers_count": 5270,
+			"friends_count": 1044,
 			"listed_count": 86,
 			"created_at": "Thu Dec 23 14:05:23 +0000 2010",
-			"favourites_count": 842,
+			"favourites_count": 844,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 3685,
+			"statuses_count": 3687,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -292,8 +205,8 @@ Grailbird.data.tweets_2018_11 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 130,
-		"favorite_count": 279,
+		"retweet_count": 131,
+		"favorite_count": 281,
 		"favorited": true,
 		"retweeted": true,
 		"scopes": {
@@ -361,16 +274,16 @@ Grailbird.data.tweets_2018_11 =
 				}
 			},
 			"protected": false,
-			"followers_count": 472950,
+			"followers_count": 473315,
 			"friends_count": 43,
-			"listed_count": 8409,
+			"listed_count": 8402,
 			"created_at": "Thu Jan 21 22:20:32 +0000 2010",
 			"favourites_count": 5,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": true,
-			"statuses_count": 6085,
+			"statuses_count": 6087,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -400,8 +313,8 @@ Grailbird.data.tweets_2018_11 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 309,
-		"favorite_count": 1165,
+		"retweet_count": 323,
+		"favorite_count": 1199,
 		"favorited": true,
 		"retweeted": true,
 		"possibly_sensitive": false,
