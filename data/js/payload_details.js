93e011d46a64df7dcd3fc727d26f4d3fd809a7d1
Keith Frankish <k.frankish@gmail.com>
2018-12-28 03:12:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1641,
-	"created_at": "Fri Dec 28 00:11:45 +0000 2018",
+	"tweets": 1642,
+	"created_at": "Fri Dec 28 01:12:46 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -997,6 +997,180 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
+			"followers_count": 61,
+			"friends_count": 659,
+			"listed_count": 1,
+			"created_at": "Fri Sep 21 09:27:31 +0000 2018",
+			"favourites_count": 34,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 55,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1055022215756988419/Vx3jdoxF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1055022215756988419/Vx3jdoxF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1043069201534119937/1541073345",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "70 years ago was adopted the Universal Declaration of Human Rights. https://t.co/2W58tIUuIY"
+	},
+	{
+		"created_at": "Thu Dec 13 17:17:34 +0000 2018",
+		"id": 1073265687811055600,
+		"id_str": "1073265687811055616",
+		"full_text": "70 years ago was adopted the Universal Declaration of Human Rights. https://t.co/2W58tIUuIY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1073265680936550400,
+					"id_str": "1073265680936550400",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/DuUBc03XQAAtgp-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DuUBc03XQAAtgp-.jpg",
+					"url": "https://t.co/2W58tIUuIY",
+					"display_url": "pic.twitter.com/2W58tIUuIY",
+					"expanded_url": "https://twitter.com/ConnectAID_int/status/1073265687811055616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1073265680936550400,
+					"id_str": "1073265680936550400",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/DuUBc03XQAAtgp-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DuUBc03XQAAtgp-.jpg",
+					"url": "https://t.co/2W58tIUuIY",
+					"display_url": "pic.twitter.com/2W58tIUuIY",
+					"expanded_url": "https://twitter.com/ConnectAID_int/status/1073265687811055616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1043069201534120000,
+			"id_str": "1043069201534119937",
+			"name": "ConnectAID",
+			"screen_name": "ConnectAID_int",
+			"location": "France",
+			"description": "The International Solidarity Network",
+			"url": "https://t.co/UEp7C2TGge",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UEp7C2TGge",
+							"expanded_url": "http://www.connectaid.com",
+							"display_url": "connectaid.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 62,
 			"friends_count": 659,
 			"listed_count": 1,
