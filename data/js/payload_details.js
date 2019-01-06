22af497d262deef14ea2401a66dce8259631dcf1
Keith Frankish <k.frankish@gmail.com>
2019-01-06 02:11:44 +0200

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1662,
-	"created_at": "Sat Jan 05 01:12:44 +0000 2019",
+	"tweets": 1665,
+	"created_at": "Sun Jan 06 00:11:44 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,173 +1,58 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
-		"created_at": "Mon Dec 31 22:31:10 +0000 2018",
-		"id": 1079867586551017500,
-		"id_str": "1079867586551017472",
-		"full_text": "I got accepted into community college today. Not a feat for most of you, but after relentless bullying in high school, I told myself I would never go back. Not a bad way to start 2019 😊",
+		"created_at": "Sat Jan 05 16:31:00 +0000 2019",
+		"id": 1081588886218911700,
+		"id_str": "1081588886218911744",
+		"full_text": "@keithfrankish There have been many. Some of the most important were André Gallois, Jonathan Dancy, Gianluigi Oliveri, George Botterill, @philosophybites, and most of all, Jenny Saul and the lovely @keithfrankish himself!",
 		"truncated": false,
 		"display_text_range": [
-			0,
-			185
-		],
-		"entities": {
-			"hashtags": [],
-			"symbols": [],
-			"user_mentions": [],
-			"urls": []
-		},
-		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
-		"in_reply_to_status_id": null,
-		"in_reply_to_status_id_str": null,
-		"in_reply_to_user_id": null,
-		"in_reply_to_user_id_str": null,
-		"in_reply_to_screen_name": null,
-		"user": {
-			"id": 4670130793,
-			"id_str": "4670130793",
-			"name": "👻 RAMSKY 👻",
-			"screen_name": "RRRamsky",
-			"location": "",
-			"description": "Rowyn • Ramsky | 2018 #PITCHWARS MENTEE! | Author of What Happened to 143 |",
-			"url": "https://t.co/E59X0Uta74",
-			"entities": {
-				"url": {
-					"urls": [
-						{
-							"url": "https://t.co/E59X0Uta74",
-							"expanded_url": "https://www.wattpad.com/user/WeHoardCats",
-							"display_url": "wattpad.com/user/WeHoardCa…",
-							"indices": [
-								0,
-								23
-							]
-						}
-					]
-				},
-				"description": {
-					"urls": []
-				}
-			},
-			"protected": false,
-			"followers_count": 911,
-			"friends_count": 715,
-			"listed_count": 10,
-			"created_at": "Mon Dec 28 23:57:08 +0000 2015",
-			"favourites_count": 6270,
-			"utc_offset": null,
-			"time_zone": null,
-			"geo_enabled": false,
-			"verified": false,
-			"statuses_count": 5954,
-			"lang": "en",
-			"contributors_enabled": false,
-			"is_translator": false,
-			"is_translation_enabled": false,
-			"profile_background_color": "000000",
-			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/1078358998015856641/ndPdRDSl_normal.jpg",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1078358998015856641/ndPdRDSl_normal.jpg",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4670130793/1529887566",
-			"profile_link_color": "ABB8C2",
-			"profile_sidebar_border_color": "000000",
-			"profile_sidebar_fill_color": "000000",
-			"profile_text_color": "000000",
-			"profile_use_background_image": false,
-			"has_extended_profile": false,
-			"default_profile": false,
-			"default_profile_image": false,
-			"following": false,
-			"follow_request_sent": false,
-			"notifications": false,
-			"translator_type": "none"
-		},
-		"geo": null,
-		"coordinates": null,
-		"place": null,
-		"contributors": null,
-		"is_quote_status": false,
-		"retweet_count": 646,
-		"favorite_count": 21131,
-		"favorited": true,
-		"retweeted": true,
-		"lang": "en",
-		"text": "I got accepted into community college today. Not a feat for most of you, but after relentless bullying in high school, I told myself I would never go back. Not a bad way to start 2019 😊"
-	},
-	{
-		"created_at": "Tue Jan 01 19:24:02 +0000 2019",
-		"id": 1080182881174147100,
-		"id_str": "1080182881174147072",
-		"full_text": "@TimAKenyon @JoshForstenzer @drangiehobbs @the_if_man @philosophyfound HNY to you too, Tim! Thanks for your kind words. I've got into P4C through teaching at the European School - philosophy is a core subject. I've a lot of practical experience but want to build up my theoretical knowledge of phil edu. It really is fascinating -- &amp; v important too!",
-		"truncated": false,
-		"display_text_range": [
-			71,
-			354
+			15,
+			221
 		],
 		"entities": {
 			"hashtags": [],
 			"symbols": [],
 			"user_mentions": [
 				{
-					"screen_name": "TimAKenyon",
-					"name": "Tim Kenyon",
-					"id": 970766939009306600,
-					"id_str": "970766939009306625",
+					"screen_name": "keithfrankish",
+					"name": "Keith Frankish",
+					"id": 52522040,
+					"id_str": "52522040",
 					"indices": [
 						0,
-						11
+						14
 					]
 				},
 				{
-					"screen_name": "JoshForstenzer",
-					"name": "Joshua Forstenzer",
-					"id": 166988769,
-					"id_str": "166988769",
+					"screen_name": "philosophybites",
+					"name": "Nigel Warburton",
+					"id": 37238862,
+					"id_str": "37238862",
 					"indices": [
-						12,
-						27
+						137,
+						153
 					]
 				},
 				{
-					"screen_name": "drangiehobbs",
-					"name": "Angie Hobbs",
-					"id": 82591098,
-					"id_str": "82591098",
+					"screen_name": "keithfrankish",
+					"name": "Keith Frankish",
+					"id": 52522040,
+					"id_str": "52522040",
 					"indices": [
-						28,
-						41
-					]
-				},
-				{
-					"screen_name": "the_if_man",
-					"name": "Peter Worley",
-					"id": 301469692,
-					"id_str": "301469692",
-					"indices": [
-						42,
-						53
-					]
-				},
-				{
-					"screen_name": "philosophyfound",
-					"name": "PhilosophyFoundation",
-					"id": 22465973,
-					"id_str": "22465973",
-					"indices": [
-						54,
-						70
+						198,
+						212
 					]
 				}
 			],
 			"urls": []
 		},
 		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
-		"in_reply_to_status_id": 1079904691478298600,
-		"in_reply_to_status_id_str": "1079904691478298624",
-		"in_reply_to_user_id": 970766939009306600,
-		"in_reply_to_user_id_str": "970766939009306625",
-		"in_reply_to_screen_name": "TimAKenyon",
+		"in_reply_to_status_id": 1081520403678408700,
+		"in_reply_to_status_id_str": "1081520403678408704",
+		"in_reply_to_user_id": 52522040,
+		"in_reply_to_user_id_str": "52522040",
+		"in_reply_to_screen_name": "keithfrankish",
 		"user": {
 			"id": 103135865,
 			"id_str": "103135865",
@@ -195,16 +80,16 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
-			"followers_count": 572,
-			"friends_count": 428,
+			"followers_count": 574,
+			"friends_count": 429,
 			"listed_count": 31,
 			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
-			"favourites_count": 2487,
+			"favourites_count": 2488,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 1422,
+			"statuses_count": 1423,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -268,11 +153,11 @@ Grailbird.data.tweets_2019_01 =
 		"contributors": null,
 		"is_quote_status": false,
 		"retweet_count": 0,
-		"favorite_count": 3,
+		"favorite_count": 4,
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@TimAKenyon @JoshForstenzer @drangiehobbs @the_if_man @philosophyfound HNY to you too, Tim! Thanks for your kind words. I've got into P4C through teaching at the European School - philosophy is a core subject. I've a lot of practical experience but want to build up my theoretical knowledge of phil edu. It really is fascinating -- &amp; v important too!"
+		"text": "@keithfrankish There have been many. Some of the most important were André Gallois, Jonathan Dancy, Gianluigi Oliveri, George Botterill, @philosophybites, and most of all, Jenny Saul and the lovely @keithfrankish himself!"
 	},
 	{
 		"created_at": "Mon Dec 31 22:31:10 +0000 2018",
@@ -323,16 +208,16 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
-			"followers_count": 911,
+			"followers_count": 915,
 			"friends_count": 715,
 			"listed_count": 10,
 			"created_at": "Mon Dec 28 23:57:08 +0000 2015",
-			"favourites_count": 6269,
+			"favourites_count": 6275,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": false,
-			"statuses_count": 5954,
+			"statuses_count": 5963,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -362,8 +247,8 @@ Grailbird.data.tweets_2019_01 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 646,
-		"favorite_count": 21130,
+		"retweet_count": 648,
+		"favorite_count": 21144,
 		"favorited": true,
 		"retweeted": true,
 		"lang": "en",
@@ -469,16 +354,16 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
-			"followers_count": 572,
-			"friends_count": 428,
+			"followers_count": 574,
+			"friends_count": 429,
 			"listed_count": 31,
 			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
-			"favourites_count": 2487,
+			"favourites_count": 2488,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 1422,
+			"statuses_count": 1423,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
