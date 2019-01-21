4c6bca683477abbdcc71677bfafdcf5ee16e4766
Keith Frankish <k.frankish@gmail.com>
2019-01-21 02:11:47 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1855,
-	"created_at": "Sun Jan 20 00:11:45 +0000 2019",
+	"tweets": 1856,
+	"created_at": "Mon Jan 21 00:11:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,127 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 15 14:27:44 +0000 2019",
+		"id": 1085181746793144300,
+		"id_str": "1086881394410819585",
+		"full_text": "A collection of advice from philosophy professors for students writing philosophy papers. #philosophy #writing https://t.co/Q5p7SMtwFJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "philosophy",
+					"indices": [
+						90,
+						101
+					]
+				},
+				{
+					"text": "writing",
+					"indices": [
+						102,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Q5p7SMtwFJ",
+					"expanded_url": "http://dailynous.com/2019/01/15/write-philosophy-paper-online-guides/",
+					"display_url": "dailynous.com/2019/01/15/wri…",
+					"indices": [
+						111,
+						134
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2386399081,
+			"id_str": "2386399081",
+			"name": "Daily Nous",
+			"screen_name": "DailyNousEditor",
+			"location": "",
+			"description": "News for and about the philosophy profession, edited by Justin Weinberg, associate professor of philosophy at the University of South Carolina.",
+			"url": "http://t.co/5Uet0c9Mdm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/5Uet0c9Mdm",
+							"expanded_url": "http://dailynous.com/",
+							"display_url": "dailynous.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13662,
+			"friends_count": 348,
+			"listed_count": 328,
+			"created_at": "Thu Mar 13 05:35:12 +0000 2014",
+			"favourites_count": 2413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4503,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/998555513917923328/o-GfJgBp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/998555513917923328/o-GfJgBp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2386399081/1501514872",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 88,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A collection of advice from philosophy professors for students writing philosophy papers. #philosophy #writing https://t.co/Q5p7SMtwFJ"
+	},
+	{
 		"created_at": "Sat Jan 19 18:31:48 +0000 2019",
 		"id": 1086692717592277000,
 		"id_str": "1086732511655444480",
