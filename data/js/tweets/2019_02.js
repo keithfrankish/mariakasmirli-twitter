357ff0f604566fc80b0e87d489b869cecbf8edf4
Keith Frankish <k.frankish@gmail.com>
2019-02-04 02:11:45 +0200

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 03 16:18:34 +0000 2019",
+		"id": 1092095008977633300,
+		"id_str": "1092146437113200640",
+		"full_text": "Don’t forget about the Teacher Training Presentation THIS TUESDAY AT 4PM in Bungalow 78✨ https://t.co/W36cE3OVqE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W36cE3OVqE",
+					"expanded_url": "https://twitter.com/keeleedusociety/status/1089582094295142401",
+					"display_url": "twitter.com/keeleedusociet…",
+					"indices": [
+						89,
+						112
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1071112111814787100,
+			"id_str": "1071112111814787072",
+			"name": "Keele Education Society",
+			"screen_name": "keeleedusociety",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12,
+			"friends_count": 9,
+			"listed_count": 0,
+			"created_at": "Fri Dec 07 18:40:02 +0000 2018",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 20,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074695157814059008/wJBaTwG0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074695157814059008/wJBaTwG0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1071112111814787072/1544208151",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1089582094295142400,
+		"quoted_status_id_str": "1089582094295142401",
+		"quoted_status_permalink": {
+			"url": "https://t.co/W36cE3OVqE",
+			"expanded": "https://twitter.com/keeleedusociety/status/1089582094295142401",
+			"display": "twitter.com/keeleedusociet…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jan 27 17:53:09 +0000 2019",
+			"id": 1089582094295142400,
+			"id_str": "1089582094295142401",
+			"full_text": "The Keele Education Society have organised a presentation by the KNSTE on Tuesday 5th February at 4-6pm in Bungalow 78.\nThe presentation will cover the different routes that Keele offer into primary school teaching. @knspsite @KeeleCareers @SSPPKeele",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				250
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "knspsite",
+						"name": "KNSTE ATs",
+						"id": 3091685290,
+						"id_str": "3091685290",
+						"indices": [
+							216,
+							225
+						]
+					},
+					{
+						"screen_name": "KeeleCareers",
+						"name": "Keele Careers",
+						"id": 36363071,
+						"id_str": "36363071",
+						"indices": [
+							226,
+							239
+						]
+					},
+					{
+						"screen_name": "SSPPKeele",
+						"name": "School of Social Science & Public Policy, Keele",
+						"id": 1027569438885912600,
+						"id_str": "1027569438885912581",
+						"indices": [
+							240,
+							250
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1071112111814787100,
+				"id_str": "1071112111814787072",
+				"name": "Keele Education Society",
+				"screen_name": "keeleedusociety",
+				"location": "",
+				"description": "",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 12,
+				"friends_count": 9,
+				"listed_count": 0,
+				"created_at": "Fri Dec 07 18:40:02 +0000 2018",
+				"favourites_count": 16,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 20,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1074695157814059008/wJBaTwG0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074695157814059008/wJBaTwG0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1071112111814787072/1544208151",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Don’t forget about the Teacher Training Presentation THIS TUESDAY AT 4PM in Bungalow 78✨ https://t.co/W36cE3OVqE"
+	},
+	{
 		"created_at": "Sat Feb 02 19:55:51 +0000 2019",
 		"id": 1091787299908657200,
 		"id_str": "1091787299908657152",
