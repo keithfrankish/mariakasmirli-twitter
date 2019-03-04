90896d854d2d20fd8751751534ea81a842c2cce8
Keith Frankish <k.frankish@gmail.com>
2019-03-04 02:11:45 +0200

    Added 2 tweets

---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,455 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Sun Mar 03 20:03:01 +0000 2019",
+		"id": 1102298350840438800,
+		"id_str": "1102298350840438784",
+		"full_text": "A luxury stay in Crete  https://t.co/9CqO64i7nI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9CqO64i7nI",
+					"expanded_url": "https://www.manchestereveningnews.co.uk/trips-and-breaks/castello-boutique-resort-spa-crete-15699908?utm_source=twitter.com&utm_medium=social&utm_campaign=sharebar",
+					"display_url": "manchestereveningnews.co.uk/trips-and-brea…",
+					"indices": [
+						24,
+						47
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 596,
+			"friends_count": 443,
+			"listed_count": 31,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 2645,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1493,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1547567920",
+			"profile_link_color": "22262A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A luxury stay in Crete  https://t.co/9CqO64i7nI"
+	},
+	{
+		"created_at": "Sun Mar 03 19:57:54 +0000 2019",
+		"id": 1102297064355053600,
+		"id_str": "1102297064355053570",
+		"full_text": "Wow! https://t.co/Jbu7QaHKkl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Jbu7QaHKkl",
+					"expanded_url": "https://twitter.com/GraHodgetts/status/1102288712359124994",
+					"display_url": "twitter.com/GraHodgetts/st…",
+					"indices": [
+						5,
+						28
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 596,
+			"friends_count": 443,
+			"listed_count": 31,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 2645,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1493,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1547567920",
+			"profile_link_color": "22262A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1102288712359125000,
+		"quoted_status_id_str": "1102288712359124994",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Jbu7QaHKkl",
+			"expanded": "https://twitter.com/GraHodgetts/status/1102288712359124994",
+			"display": "twitter.com/GraHodgetts/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Mar 03 19:24:43 +0000 2019",
+			"id": 1102288712359125000,
+			"id_str": "1102288712359124994",
+			"full_text": "Well this one is going to take some explaining. Looks like an album cover. Band names on a postcard please. Actually a scene from a carnival in Krousonas, Crete today. #streetphotography #streetphoto #carnival #greekcarnival #fujixt3 #fujifilmxt3 #xf23mm… https://t.co/Z6hBtD4On2 https://t.co/pMEB9Ex5vA",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				279
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "streetphotography",
+						"indices": [
+							168,
+							186
+						]
+					},
+					{
+						"text": "streetphoto",
+						"indices": [
+							187,
+							199
+						]
+					},
+					{
+						"text": "carnival",
+						"indices": [
+							200,
+							209
+						]
+					},
+					{
+						"text": "greekcarnival",
+						"indices": [
+							210,
+							224
+						]
+					},
+					{
+						"text": "fujixt3",
+						"indices": [
+							225,
+							233
+						]
+					},
+					{
+						"text": "fujifilmxt3",
+						"indices": [
+							234,
+							246
+						]
+					},
+					{
+						"text": "xf23mm",
+						"indices": [
+							247,
+							254
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Z6hBtD4On2",
+						"expanded_url": "https://ift.tt/2C27490",
+						"display_url": "ift.tt/2C27490",
+						"indices": [
+							256,
+							279
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1102288711163801600,
+						"id_str": "1102288711163801600",
+						"indices": [
+							280,
+							303
+						],
+						"media_url": "http://pbs.twimg.com/media/D0wdvhzXcAAdGIg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D0wdvhzXcAAdGIg.jpg",
+						"url": "https://t.co/pMEB9Ex5vA",
+						"display_url": "pic.twitter.com/pMEB9Ex5vA",
+						"expanded_url": "https://twitter.com/GraHodgetts/status/1102288712359124994/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1102288711163801600,
+						"id_str": "1102288711163801600",
+						"indices": [
+							280,
+							303
+						],
+						"media_url": "http://pbs.twimg.com/media/D0wdvhzXcAAdGIg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D0wdvhzXcAAdGIg.jpg",
+						"url": "https://t.co/pMEB9Ex5vA",
+						"display_url": "pic.twitter.com/pMEB9Ex5vA",
+						"expanded_url": "https://twitter.com/GraHodgetts/status/1102288712359124994/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 426,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 68189879,
+				"id_str": "68189879",
+				"name": "Graham",
+				"screen_name": "GraHodgetts",
+				"location": "Crete, Greece",
+				"description": "Reportage wedding photographer based in Greece. A father & a runner, Welsh & a wine lover. Landscapes & Street photography in spare time.",
+				"url": "https://t.co/oAIvPT1SCT",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/oAIvPT1SCT",
+								"expanded_url": "http://www.grahamhodgetts.com",
+								"display_url": "grahamhodgetts.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 222,
+				"friends_count": 281,
+				"listed_count": 32,
+				"created_at": "Sun Aug 23 17:23:38 +0000 2009",
+				"favourites_count": 434,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4551,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "333233",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1090578685630078976/sKbRadHn_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090578685630078976/sKbRadHn_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/68189879/1527098228",
+				"profile_link_color": "D1615E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E6E6E6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "Wow! https://t.co/Jbu7QaHKkl"
+	}
+]
