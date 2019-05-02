e5ccbdb160f98215aa20262be1b0a26b8423c58f
Keith Frankish <k.frankish@gmail.com>
2019-05-02 03:11:46 +0300

    Added 1 tweets

---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,340 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 14:01:19 +0000 2019",
+		"id": 1123579189905494000,
+		"id_str": "1123588211333390338",
+		"full_text": "Ritsos was one of the greatest Greek poets of all time. He was never considered for a Nobel prize    for political reasons. He did get the Lenin prize instead. His Epitaph, a poem for the dead workers in the 1st of May 1936 strike and demonstration, is a masterpiece. https://t.co/vPOCPLIUqw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vPOCPLIUqw",
+					"expanded_url": "https://twitter.com/johnkittmer/status/1123535894898774016",
+					"display_url": "twitter.com/johnkittmer/st…",
+					"indices": [
+						268,
+						291
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3068008811,
+			"id_str": "3068008811",
+			"name": "Stathis Psillos",
+			"screen_name": "StathisPsillos",
+			"location": "Athens Greece",
+			"description": "I am a philosopher, lucky enough to do philosophy for a living. I tweet about philosophy, and occasionally politics",
+			"url": "https://t.co/zUI0aPk6f3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zUI0aPk6f3",
+							"expanded_url": "http://www.phs.uoa.gr/psillos",
+							"display_url": "phs.uoa.gr/psillos",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2962,
+			"friends_count": 681,
+			"listed_count": 78,
+			"created_at": "Tue Mar 03 15:27:52 +0000 2015",
+			"favourites_count": 6053,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8632,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/572783820958769152/VZGcYbIt_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/572783820958769152/VZGcYbIt_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3068008811/1549239146",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1123535894898774000,
+		"quoted_status_id_str": "1123535894898774016",
+		"quoted_status_permalink": {
+			"url": "https://t.co/vPOCPLIUqw",
+			"expanded": "https://twitter.com/johnkittmer/status/1123535894898774016",
+			"display": "twitter.com/johnkittmer/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed May 01 10:33:26 +0000 2019",
+			"id": 1123535894898774000,
+			"id_str": "1123535894898774016",
+			"full_text": "On this day, in 1909, was born the great poet #YannisRitsos. Champion of the left, fearless explorer of human doubt and fragility. A unique voice in international poetry. This poem from “Gestures”, tr. #NikosStangos https://t.co/IN2ohZkVZz",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				215
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "YannisRitsos",
+						"indices": [
+							46,
+							59
+						]
+					},
+					{
+						"text": "NikosStangos",
+						"indices": [
+							202,
+							215
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1123535878004117500,
+						"id_str": "1123535878004117504",
+						"indices": [
+							216,
+							239
+						],
+						"media_url": "http://pbs.twimg.com/media/D5eZ7aTXkAAaf8S.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5eZ7aTXkAAaf8S.jpg",
+						"url": "https://t.co/IN2ohZkVZz",
+						"display_url": "pic.twitter.com/IN2ohZkVZz",
+						"expanded_url": "https://twitter.com/JohnKittmer/status/1123535894898774016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1123535878004117500,
+						"id_str": "1123535878004117504",
+						"indices": [
+							216,
+							239
+						],
+						"media_url": "http://pbs.twimg.com/media/D5eZ7aTXkAAaf8S.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5eZ7aTXkAAaf8S.jpg",
+						"url": "https://t.co/IN2ohZkVZz",
+						"display_url": "pic.twitter.com/IN2ohZkVZz",
+						"expanded_url": "https://twitter.com/JohnKittmer/status/1123535894898774016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1123535878008209400,
+						"id_str": "1123535878008209408",
+						"indices": [
+							216,
+							239
+						],
+						"media_url": "http://pbs.twimg.com/media/D5eZ7aUWAAAApel.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5eZ7aUWAAAApel.jpg",
+						"url": "https://t.co/IN2ohZkVZz",
+						"display_url": "pic.twitter.com/IN2ohZkVZz",
+						"expanded_url": "https://twitter.com/JohnKittmer/status/1123535894898774016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 372,
+								"h": 400,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 372,
+								"h": 400,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 372,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 847892022,
+				"id_str": "847892022",
+				"name": "John Kittmer",
+				"screen_name": "JohnKittmer",
+				"location": "Tooting, London",
+				"description": "Chair @AngHellenLeague. Northerner. Interests in Greece, maritime policy, social justice, ecology, gardening, poetry, Christian faith, JRTs.",
+				"url": "https://t.co/gKJm6XXZHH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gKJm6XXZHH",
+								"expanded_url": "https://antapokriseiscorrespondences.blogspot.com",
+								"display_url": "…pokriseiscorrespondences.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4535,
+				"friends_count": 201,
+				"listed_count": 143,
+				"created_at": "Wed Sep 26 18:34:08 +0000 2012",
+				"favourites_count": 6410,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 12618,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "98A5AB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1114178782519283713/X1AIQWmQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1114178782519283713/X1AIQWmQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/847892022/1554475900",
+				"profile_link_color": "FAB81E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 17,
+			"favorite_count": 62,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 8,
+		"favorite_count": 11,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ritsos was one of the greatest Greek poets of all time. He was never considered for a Nobel prize    for political reasons. He did get the Lenin prize instead. His Epitaph, a poem for the dead workers in the 1st of May 1936 strike and demonstration, is a masterpiece. https://t.co/vPOCPLIUqw"
+	}
+]
