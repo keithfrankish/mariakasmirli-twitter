3b0e809fce6a141c489bd716133a22226b1cbf09
Keith Frankish <k.frankish@gmail.com>
2019-06-02 03:11:45 +0300

    Added 4 tweets

---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,412 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Sat Jun 01 19:34:33 +0000 2019",
+		"id": 1134901368450375700,
+		"id_str": "1134906095661670400",
+		"full_text": "Is Philosophy OK? https://t.co/9WBJwa03dn via @electricagora",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ElectricAgora",
+					"name": "The Electric Agora",
+					"id": 3505472194,
+					"id_str": "3505472194",
+					"indices": [
+						46,
+						60
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9WBJwa03dn",
+					"expanded_url": "https://theelectricagora.com/2019/05/23/is-philosophy-ok/",
+					"display_url": "theelectricagora.com/2019/05/23/is-…",
+					"indices": [
+						18,
+						41
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 416608486,
+			"id_str": "416608486",
+			"name": "Judith Stout",
+			"screen_name": "judystout1",
+			"location": "usa",
+			"description": "Univ of Iowa grad, married, seeking new thought patterns in changing world.Read Philosophy, Hist, Politics. Interested in everything",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3513,
+			"friends_count": 3588,
+			"listed_count": 184,
+			"created_at": "Sat Nov 19 22:43:29 +0000 2011",
+			"favourites_count": 3525,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 78136,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2259272648/IMG0010_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2259272648/IMG0010_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Is Philosophy OK? https://t.co/9WBJwa03dn via @electricagora"
+	},
+	{
+		"created_at": "Sat Jun 01 14:24:32 +0000 2019",
+		"id": 1134506860759961600,
+		"id_str": "1134828079271071746",
+		"full_text": "Some people think that since I left academia I’ve become more of a journalist than a philosopher, yet I think I’ve been doing more philosophy outside that weird and narrow research’ culture than was possible within. I may be wrong, of course.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			242
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37238862,
+			"id_str": "37238862",
+			"name": "Nigel Warburton",
+			"screen_name": "philosophybites",
+			"location": "Oxford  & London UK",
+			"description": "Freelance philosopher. A Little History of Philosophy, Philosophy Bites. Consultant editor @aeonmag, @thenewphil, @five_books. #philosophyinthebookshop",
+			"url": "https://t.co/kD7ridjs8Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kD7ridjs8Q",
+							"expanded_url": "http://www.nigelwarburton.com",
+							"display_url": "nigelwarburton.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 73147,
+			"friends_count": 6274,
+			"listed_count": 2522,
+			"created_at": "Sat May 02 17:20:21 +0000 2009",
+			"favourites_count": 34460,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 42341,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B5B8B4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37238862/1398336718",
+			"profile_link_color": "000DFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 233,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Some people think that since I left academia I’ve become more of a journalist than a philosopher, yet I think I’ve been doing more philosophy outside that weird and narrow research’ culture than was possible within. I may be wrong, of course."
+	},
+	{
+		"created_at": "Sat Jun 01 14:21:23 +0000 2019",
+		"id": 1134794706162999300,
+		"id_str": "1134827285146722305",
+		"full_text": "@philosophybites I would make a much stronger claim. You’ve done more for philosophy! If, 20 years from now, there will be any philosophy departments at all outside of elite universities, it will largely be because a case for the value of philosophy has been made outside of specialist circles.",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			294
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "philosophybites",
+					"name": "Nigel Warburton",
+					"id": 37238862,
+					"id_str": "37238862",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1134506860759961600,
+		"in_reply_to_status_id_str": "1134506860759961601",
+		"in_reply_to_user_id": 37238862,
+		"in_reply_to_user_id_str": "37238862",
+		"in_reply_to_screen_name": "philosophybites",
+		"user": {
+			"id": 182101428,
+			"id_str": "182101428",
+			"name": "Evan Selinger",
+			"screen_name": "EvanSelinger",
+			"location": "",
+			"description": "Philosophy Professor at RIT. \"Re-Engineering Humanity\" & \"Cambridge Handbook of Consumer Privacy\" (2018). Bylines everywhere.",
+			"url": "https://t.co/RdZcPCMdKL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RdZcPCMdKL",
+							"expanded_url": "http://eselinger.org/",
+							"display_url": "eselinger.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7102,
+			"friends_count": 1987,
+			"listed_count": 436,
+			"created_at": "Mon Aug 23 20:54:31 +0000 2010",
+			"favourites_count": 10745,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15585,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102974460334952448/8hrl3WDu_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102974460334952448/8hrl3WDu_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/182101428/1521745619",
+			"profile_link_color": "002699",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@philosophybites I would make a much stronger claim. You’ve done more for philosophy! If, 20 years from now, there will be any philosophy departments at all outside of elite universities, it will largely be because a case for the value of philosophy has been made outside of specialist circles."
+	},
+	{
+		"created_at": "Sat Jun 01 14:20:36 +0000 2019",
+		"id": 1134545331574759400,
+		"id_str": "1134827087796277248",
+		"full_text": "@philosophybites I think you are right. Philosophy should be for anyone who is interested in thinking.",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "philosophybites",
+					"name": "Nigel Warburton",
+					"id": 37238862,
+					"id_str": "37238862",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1134506860759961600,
+		"in_reply_to_status_id_str": "1134506860759961601",
+		"in_reply_to_user_id": 37238862,
+		"in_reply_to_user_id_str": "37238862",
+		"in_reply_to_screen_name": "philosophybites",
+		"user": {
+			"id": 82719597,
+			"id_str": "82719597",
+			"name": "Ron Green",
+			"screen_name": "Rongreen5",
+			"location": "",
+			"description": "Linguist, philosopher, author. \"Time to Tell: a look at how we tick\" (iff Books, 2018). \"Nothing Matters: a book about nothing\" (iff-Books, 2011)",
+			"url": "https://t.co/3VdRE0Frfv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3VdRE0Frfv",
+							"expanded_url": "http://www.nothing-matters.org",
+							"display_url": "nothing-matters.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 225,
+			"friends_count": 523,
+			"listed_count": 4,
+			"created_at": "Thu Oct 15 21:39:56 +0000 2009",
+			"favourites_count": 1811,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1105002805159440386/5zGUIKFw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1105002805159440386/5zGUIKFw_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@philosophybites I think you are right. Philosophy should be for anyone who is interested in thinking."
+	}
+]
