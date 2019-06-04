f3bf0418f9e9c6946d271c1ae7cd64d6c67f951c
Keith Frankish <k.frankish@gmail.com>
2019-06-04 03:11:45 +0300

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1930,
-	"created_at": "Sun Jun 02 00:11:44 +0000 2019",
+	"tweets": 1933,
+	"created_at": "Tue Jun 04 00:11:44 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,465 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 14:01:46 +0000 2019",
+		"id": 1135547123846570000,
+		"id_str": "1135547123846569985",
+		"full_text": "Knowledge is a stone-age concept, we're better off without it – https://t.co/DQG9DZ5kKX via @aeonmag",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "aeonmag",
+					"name": "Aeon",
+					"id": 481943972,
+					"id_str": "481943972",
+					"indices": [
+						92,
+						100
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DQG9DZ5kKX",
+					"expanded_url": "https://aeon.co/essays/knowledge-is-a-stone-age-concept-were-better-off-without-it",
+					"display_url": "aeon.co/essays/knowled…",
+					"indices": [
+						64,
+						87
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 637,
+			"friends_count": 457,
+			"listed_count": 31,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 2882,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1561,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1083088903102480387/2rLffYvL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1547567920",
+			"profile_link_color": "22262A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Knowledge is a stone-age concept, we're better off without it – https://t.co/DQG9DZ5kKX via @aeonmag"
+	},
+	{
+		"created_at": "Mon Jun 03 14:00:03 +0000 2019",
+		"id": 1135472739870040000,
+		"id_str": "1135546691766165505",
+		"full_text": "Students and staff tell us what it's like to study Philosophy at Sheffield in this video. Find out more at our Open Day on 22nd June! #mydayatsheffield #openday https://t.co/rMcKj8lGYW https://t.co/0TcuDfGTqZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			184
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mydayatsheffield",
+					"indices": [
+						134,
+						151
+					]
+				},
+				{
+					"text": "openday",
+					"indices": [
+						152,
+						160
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rMcKj8lGYW",
+					"expanded_url": "http://www.sheffield.ac.uk/undergraduate/opendays",
+					"display_url": "sheffield.ac.uk/undergraduate/…",
+					"indices": [
+						161,
+						184
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1135472578942951400,
+					"id_str": "1135472578942951424",
+					"indices": [
+						185,
+						208
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1135472578942951424/pu/img/8pUkxgOSsF_V8ns9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1135472578942951424/pu/img/8pUkxgOSsF_V8ns9.jpg",
+					"url": "https://t.co/0TcuDfGTqZ",
+					"display_url": "pic.twitter.com/0TcuDfGTqZ",
+					"expanded_url": "https://twitter.com/Sheffphilosophy/status/1135472739870040064/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135472578942951400,
+					"id_str": "1135472578942951424",
+					"indices": [
+						185,
+						208
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1135472578942951424/pu/img/8pUkxgOSsF_V8ns9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1135472578942951424/pu/img/8pUkxgOSsF_V8ns9.jpg",
+					"url": "https://t.co/0TcuDfGTqZ",
+					"display_url": "pic.twitter.com/0TcuDfGTqZ",
+					"expanded_url": "https://twitter.com/Sheffphilosophy/status/1135472739870040064/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 45000,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1135472578942951424/pu/vid/640x360/he7yCis3zAsq0PvW.mp4?tag=9"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1135472578942951424/pu/pl/wc1XvcuQl67p4Bu3.m3u8?tag=9"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1135472578942951424/pu/vid/320x180/iHN717PaT6gLAjjm.mp4?tag=9"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1135472578942951424/pu/vid/1280x720/L1EMAjaOSd49kp5E.mp4?tag=9"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2812814696,
+			"id_str": "2812814696",
+			"name": "Sheffield Philosophy",
+			"screen_name": "Sheffphilosophy",
+			"location": "45 Victoria Street, Sheffield",
+			"description": "Dept. of Philosophy at the University of Sheffield. Top two in UK in REF 2014 for quality of publications and internationally excellent research.",
+			"url": "http://t.co/rS43g4UbLN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/rS43g4UbLN",
+							"expanded_url": "http://www.sheffield.ac.uk/philosophy",
+							"display_url": "sheffield.ac.uk/philosophy",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1801,
+			"friends_count": 495,
+			"listed_count": 44,
+			"created_at": "Tue Sep 16 09:48:37 +0000 2014",
+			"favourites_count": 296,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3849,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/877450774946729984/5pQtplTL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/877450774946729984/5pQtplTL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2812814696/1535465206",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Students and staff tell us what it's like to study Philosophy at Sheffield in this video. Find out more at our Open Day on 22nd June! #mydayatsheffield #openday https://t.co/rMcKj8lGYW https://t.co/0TcuDfGTqZ"
+	},
+	{
+		"created_at": "Mon Jun 03 05:30:24 +0000 2019",
+		"id": 1135246326004879400,
+		"id_str": "1135418436081897472",
+		"full_text": "6 Socratic Questioning Tips💡for Revising with Students \n\n02.06.19\n\nLive video 🎥 by @TeacherToolkit \n\n#TTkitVLOG  https://t.co/SoeuD0os07",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TTkitVLOG",
+					"indices": [
+						101,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeacherToolkit",
+					"name": "🇬🇧 TeacherToolkit.co.uk",
+					"id": 180327352,
+					"id_str": "180327352",
+					"indices": [
+						83,
+						98
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/SoeuD0os07",
+					"expanded_url": "https://www.pscp.tv/w/b8QHojU2ODQwN3wxdkFHUldMUXZvWUps17e6jzPEgQUInwGxM2uk7N9y7UvNS774RjD3hOPdxbg=",
+					"display_url": "pscp.tv/w/b8QHojU2ODQw…",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 180327352,
+			"id_str": "180327352",
+			"name": "🇬🇧 TeacherToolkit.co.uk",
+			"screen_name": "TeacherToolkit",
+			"location": "United Kingdom",
+			"description": "No.1 Education Blog 🇬🇧 UK ￼| Author 📚 | CPD Trainer | Exp. School Leader | Teaching 25 Years | Blogger 📲 | Influencer 🚀 | #EdD Researcher 🔬 @Cambridge_Uni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 212837,
+			"friends_count": 897,
+			"listed_count": 2372,
+			"created_at": "Thu Aug 19 10:43:32 +0000 2010",
+			"favourites_count": 72923,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 143959,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EA1F4C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1129298125385084929/mUC1NpsC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1129298125385084929/mUC1NpsC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/180327352/1537955109",
+			"profile_link_color": "EA1F4C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 49,
+		"favorite_count": 105,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "6 Socratic Questioning Tips💡for Revising with Students \n\n02.06.19\n\nLive video 🎥 by @TeacherToolkit \n\n#TTkitVLOG  https://t.co/SoeuD0os07"
+	},
+	{
 		"created_at": "Sat Jun 01 19:34:33 +0000 2019",
 		"id": 1134901368450375700,
 		"id_str": "1134906095661670400",
