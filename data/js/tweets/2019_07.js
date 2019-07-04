4426bdc4d13d469454b4d8dcc1e45ef2e0a6316e
Keith Frankish <k.frankish@gmail.com>
2019-07-04 03:11:45 +0300

    Added 2 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,361 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Wed Jul 03 13:39:22 +0000 2019",
+		"id": 1146123278219657200,
+		"id_str": "1146413123441758208",
+		"full_text": "Our fantastic PGCE students completed the Foundation Level One Course in Philosophy for Children today. Congratulations to you all! 🎉 Thank you to the wonderful Sue @dialoguewks #Sapere #P4C #philosophy \n\nWell done NQTs 👏👏,  now go and enjoy the summer 😎! https://t.co/hEsUYbo5Vs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Sapere",
+					"indices": [
+						178,
+						185
+					]
+				},
+				{
+					"text": "P4C",
+					"indices": [
+						186,
+						190
+					]
+				},
+				{
+					"text": "philosophy",
+					"indices": [
+						191,
+						202
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dialoguewks",
+					"name": "DialogueWorks",
+					"id": 979782162131112000,
+					"id_str": "979782162131111937",
+					"indices": [
+						165,
+						177
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146123254828077000,
+					"id_str": "1146123254828077060",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/D-fZByDXkAQ6neM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-fZByDXkAQ6neM.jpg",
+					"url": "https://t.co/hEsUYbo5Vs",
+					"display_url": "pic.twitter.com/hEsUYbo5Vs",
+					"expanded_url": "https://twitter.com/JudePenny2/status/1146123278219657223/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146123254828077000,
+					"id_str": "1146123254828077060",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/D-fZByDXkAQ6neM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-fZByDXkAQ6neM.jpg",
+					"url": "https://t.co/hEsUYbo5Vs",
+					"display_url": "pic.twitter.com/hEsUYbo5Vs",
+					"expanded_url": "https://twitter.com/JudePenny2/status/1146123278219657223/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1146123268082077700,
+					"id_str": "1146123268082077696",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/D-fZCjbXkAA3zGZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-fZCjbXkAA3zGZ.jpg",
+					"url": "https://t.co/hEsUYbo5Vs",
+					"display_url": "pic.twitter.com/hEsUYbo5Vs",
+					"expanded_url": "https://twitter.com/JudePenny2/status/1146123278219657223/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 717126734286553100,
+			"id_str": "717126734286553092",
+			"name": "Jude Penny",
+			"screen_name": "JudePenny2",
+			"location": "Cardiff, Wales",
+			"description": "Senior Lecturer ITE. Sci, P4C, PSHE,   \n'there's all the difference between having something to say and having to say something' (Dewey - not the library guy).",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143,
+			"friends_count": 629,
+			"listed_count": 2,
+			"created_at": "Mon Apr 04 23:08:31 +0000 2016",
+			"favourites_count": 994,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 569,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117328290585436160/CD03zyVM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117328290585436160/CD03zyVM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/717126734286553092/1539811322",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 24,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our fantastic PGCE students completed the Foundation Level One Course in Philosophy for Children today. Congratulations to you all! 🎉 Thank you to the wonderful Sue @dialoguewks #Sapere #P4C #philosophy \n\nWell done NQTs 👏👏,  now go and enjoy the summer 😎! https://t.co/hEsUYbo5Vs"
+	},
+	{
+		"created_at": "Wed Jul 03 13:39:00 +0000 2019",
+		"id": 1146177227811373000,
+		"id_str": "1146413030516936705",
+		"full_text": "Eating the bloated liver of a force-fed duck or goose sounds like something out of Hannibal 😱\n \nFoie gras is so cruel that it’s even illegal in many countries—there’s NO reason for @Marriott to serve it! \n\nTell #Marriott to drop foie gras from its menu 👇 https://t.co/DZxAUoH4wY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Marriott",
+					"indices": [
+						211,
+						220
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Marriott",
+					"name": "Marriott Hotels",
+					"id": 14369314,
+					"id_str": "14369314",
+					"indices": [
+						181,
+						190
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DZxAUoH4wY",
+					"expanded_url": "http://peta.vg/2j1n",
+					"display_url": "peta.vg/2j1n",
+					"indices": [
+						255,
+						278
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9890492,
+			"id_str": "9890492",
+			"name": "PETA",
+			"screen_name": "peta",
+			"location": "Los Angeles, CA / Norfolk, VA",
+			"description": "Breaking animal news, #vegan recipes, rescues, & more from the largest animal rights organization in the world.",
+			"url": "https://t.co/5exYWceR2c",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5exYWceR2c",
+							"expanded_url": "http://peta.org",
+							"display_url": "peta.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1078862,
+			"friends_count": 5026,
+			"listed_count": 8819,
+			"created_at": "Fri Nov 02 16:29:30 +0000 2007",
+			"favourites_count": 91765,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 393521,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4BC1E0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1145702495080996865/hqbOTgub_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1145702495080996865/hqbOTgub_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9890492/1558388591",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "09090A",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 90,
+		"favorite_count": 169,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Eating the bloated liver of a force-fed duck or goose sounds like something out of Hannibal 😱\n \nFoie gras is so cruel that it’s even illegal in many countries—there’s NO reason for @Marriott to serve it! \n\nTell #Marriott to drop foie gras from its menu 👇 https://t.co/DZxAUoH4wY"
+	},
+	{
 		"created_at": "Tue Jul 02 15:35:03 +0000 2019",
 		"id": 1146079847258738700,
 		"id_str": "1146079847258738689",
