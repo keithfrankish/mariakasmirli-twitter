5149b633375f0573ad1a05a69a435276f3c59ee3
Keith Frankish <k.frankish@gmail.com>
2019-09-16 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1969,
-	"created_at": "Mon Sep 09 00:11:44 +0000 2019",
+	"tweets": 1970,
+	"created_at": "Mon Sep 16 00:11:44 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 15 17:00:47 +0000 2019",
+		"id": 1171754116521103400,
+		"id_str": "1173280512749768704",
+		"full_text": "I am proud and honored to continue serving as @ICPIC_P4C  President on the newly elected Executive Committee for 2019-2021\nTogether with the other members, we will continue to promote the free spirit around the world for philosophical inquiry with children.\n\n#P4C \n#PwC https://t.co/VV4OEu9OQC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "P4C",
+					"indices": [
+						259,
+						263
+					]
+				},
+				{
+					"text": "PwC",
+					"indices": [
+						265,
+						269
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ICPIC_P4C",
+					"name": "ICPIC_P4C",
+					"id": 2463633734,
+					"id_str": "2463633734",
+					"indices": [
+						46,
+						56
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1171754111198552000,
+					"id_str": "1171754111198552070",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/EELoJ-oX4AY4BW4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EELoJ-oX4AY4BW4.jpg",
+					"url": "https://t.co/VV4OEu9OQC",
+					"display_url": "pic.twitter.com/VV4OEu9OQC",
+					"expanded_url": "https://twitter.com/KizelArie/status/1171754116521103360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1171754111198552000,
+					"id_str": "1171754111198552070",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/EELoJ-oX4AY4BW4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EELoJ-oX4AY4BW4.jpg",
+					"url": "https://t.co/VV4OEu9OQC",
+					"display_url": "pic.twitter.com/VV4OEu9OQC",
+					"expanded_url": "https://twitter.com/KizelArie/status/1171754116521103360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 380,
+							"h": 380,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1932238772,
+			"id_str": "1932238772",
+			"name": "Dr. Arie Kizel",
+			"screen_name": "KizelArie",
+			"location": "Israel",
+			"description": "President of @ICPIC_P4C ; Head, Development of Educational Systems Program at the @haifaUniversity; Philosophy of Education #P4C #PwC Textbooks & Narrative",
+			"url": "https://t.co/OPYlsaj8BS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OPYlsaj8BS",
+							"expanded_url": "http://ariekizel.blogspot.co.il/",
+							"display_url": "ariekizel.blogspot.co.il",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2475,
+			"friends_count": 1240,
+			"listed_count": 62,
+			"created_at": "Thu Oct 03 22:22:37 +0000 2013",
+			"favourites_count": 18275,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8087,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171110703874433026/XFhJA2yV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171110703874433026/XFhJA2yV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1932238772/1565361476",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 16,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I am proud and honored to continue serving as @ICPIC_P4C  President on the newly elected Executive Committee for 2019-2021\nTogether with the other members, we will continue to promote the free spirit around the world for philosophical inquiry with children.\n\n#P4C \n#PwC https://t.co/VV4OEu9OQC"
+	},
+	{
 		"created_at": "Sun Sep 08 20:19:12 +0000 2019",
 		"id": 1170793729705357300,
 		"id_str": "1170793729705357312",
