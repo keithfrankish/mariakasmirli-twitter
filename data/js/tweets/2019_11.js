f211558da0bc7bbc053faf7fd26f2ff114a721de
Keith Frankish <k.frankish@gmail.com>
2019-11-05 02:11:45 +0200

    Added 1 tweets

---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,228 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Mon Nov 04 14:29:04 +0000 2019",
+		"id": 1191196761810862000,
+		"id_str": "1191361727260217344",
+		"full_text": "”Information technology allow a better and more efficient dissemination of falsehoods than ever.” CENS Workshop on Understanding and Countering Online Falsehoods and Influence Operations :The New Disruptive Media and Southeast and East Asia by @MarkkuMantila @Faktabaari https://t.co/VS71QkZQQV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MarkkuMantila",
+					"name": "Markku Mantila",
+					"id": 2892470224,
+					"id_str": "2892470224",
+					"indices": [
+						244,
+						258
+					]
+				},
+				{
+					"screen_name": "Faktabaari",
+					"name": "Faktabaari",
+					"id": 2325242556,
+					"id_str": "2325242556",
+					"indices": [
+						259,
+						270
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1191196747420160000,
+					"id_str": "1191196747420160001",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EIf7IWuUUAEDAze.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIf7IWuUUAEDAze.jpg",
+					"url": "https://t.co/VS71QkZQQV",
+					"display_url": "pic.twitter.com/VS71QkZQQV",
+					"expanded_url": "https://twitter.com/Kari_Kivinen/status/1191196761810862080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 398,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1191196747420160000,
+					"id_str": "1191196747420160001",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EIf7IWuUUAEDAze.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIf7IWuUUAEDAze.jpg",
+					"url": "https://t.co/VS71QkZQQV",
+					"display_url": "pic.twitter.com/VS71QkZQQV",
+					"expanded_url": "https://twitter.com/Kari_Kivinen/status/1191196761810862080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 703,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 398,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 77735405,
+			"id_str": "77735405",
+			"name": "Kari Kivinen",
+			"screen_name": "Kari_Kivinen",
+			"location": "Helsinki, Suomi",
+			"description": "Interested in media & information literacy, motivation research, self-regulation, multilingualism, IP-education, IT, languages, French, etc.",
+			"url": "https://t.co/sKuluJBIXh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sKuluJBIXh",
+							"expanded_url": "http://kivinen.wordpress.com",
+							"display_url": "kivinen.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 677,
+			"friends_count": 1009,
+			"listed_count": 24,
+			"created_at": "Sun Sep 27 13:29:13 +0000 2009",
+			"favourites_count": 8208,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3875,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102121621995827200/Mk7DxwdQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102121621995827200/Mk7DxwdQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/77735405/1551601330",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 5,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "”Information technology allow a better and more efficient dissemination of falsehoods than ever.” CENS Workshop on Understanding and Countering Online Falsehoods and Influence Operations :The New Disruptive Media and Southeast and East Asia by @MarkkuMantila @Faktabaari https://t.co/VS71QkZQQV"
+	}
+]
