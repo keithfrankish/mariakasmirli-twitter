0e6484cd91b918b8f13b612c0b2eb7c9b6f33442
Keith Frankish <k.frankish@gmail.com>
2019-11-16 02:11:45 +0200

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 15 06:27:04 +0000 2019",
+		"id": 1195225424365858800,
+		"id_str": "1195226693243408386",
+		"full_text": "In case you missed it: Heads calling for revolution in SEND support    https://t.co/xQhvlhqr6z https://t.co/BXlxHY6zV7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xQhvlhqr6z",
+					"expanded_url": "http://bit.ly/33Pengb",
+					"display_url": "bit.ly/33Pengb",
+					"indices": [
+						71,
+						94
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1195225422788800500,
+					"id_str": "1195225422788800513",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EJZLMH_XsAEb8rV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJZLMH_XsAEb8rV.jpg",
+					"url": "https://t.co/BXlxHY6zV7",
+					"display_url": "pic.twitter.com/BXlxHY6zV7",
+					"expanded_url": "https://twitter.com/SchoolsImprove/status/1195225424365858816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 682,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1195225422788800500,
+					"id_str": "1195225422788800513",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EJZLMH_XsAEb8rV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJZLMH_XsAEb8rV.jpg",
+					"url": "https://t.co/BXlxHY6zV7",
+					"display_url": "pic.twitter.com/BXlxHY6zV7",
+					"expanded_url": "https://twitter.com/SchoolsImprove/status/1195225424365858816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 682,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 628251837,
+			"id_str": "628251837",
+			"name": "Schools Improvement",
+			"screen_name": "SchoolsImprove",
+			"location": "",
+			"description": "http://t.co/2mgeBp3xRQ - the UK's leading site for schools news & views - sign up for free daily email bulletin. RTs ≠ endorsements. Maintained by @EduKitters",
+			"url": "http://t.co/K5xshKxM8Z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/K5xshKxM8Z",
+							"expanded_url": "http://schoolsimprovement.net",
+							"display_url": "schoolsimprovement.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/2mgeBp3xRQ",
+							"expanded_url": "http://SchoolsImprovement.net",
+							"display_url": "SchoolsImprovement.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 76258,
+			"friends_count": 22486,
+			"listed_count": 944,
+			"created_at": "Fri Jul 06 08:11:06 +0000 2012",
+			"favourites_count": 263,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 130667,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/595219916803940354/w3PoRo3P_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/595219916803940354/w3PoRo3P_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In case you missed it: Heads calling for revolution in SEND support    https://t.co/xQhvlhqr6z https://t.co/BXlxHY6zV7"
+	},
+	{
 		"created_at": "Wed Nov 13 16:38:29 +0000 2019",
 		"id": 1194635536603836400,
 		"id_str": "1194655786808619008",
