b4183178902c2c3eed26084cf28a97e8ccb43265
Keith Frankish <k.frankish@gmail.com>
2019-12-06 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2007,
-	"created_at": "Tue Dec 03 00:11:44 +0000 2019",
+	"tweets": 2008,
+	"created_at": "Fri Dec 06 00:11:44 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 05 16:31:54 +0000 2019",
+		"id": 1202598468868550700,
+		"id_str": "1202626660119519232",
+		"full_text": "Our very own Professor Rosanna Keefe, is back from Spain, where she was a keynote speaker at the Conference of the Spanish Society for Analytic Philosophy in Valencia! 🇪🇸 #IXSEFAConference https://t.co/6CqAU4D9ST",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			188
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IXSEFAConference",
+					"indices": [
+						171,
+						188
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1202598464862994400,
+					"id_str": "1202598464862994433",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/ELB87qGWwAEQ1Vv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELB87qGWwAEQ1Vv.jpg",
+					"url": "https://t.co/6CqAU4D9ST",
+					"display_url": "pic.twitter.com/6CqAU4D9ST",
+					"expanded_url": "https://twitter.com/Sheffphilosophy/status/1202598468868550663/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1202598464862994400,
+					"id_str": "1202598464862994433",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/ELB87qGWwAEQ1Vv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELB87qGWwAEQ1Vv.jpg",
+					"url": "https://t.co/6CqAU4D9ST",
+					"display_url": "pic.twitter.com/6CqAU4D9ST",
+					"expanded_url": "https://twitter.com/Sheffphilosophy/status/1202598468868550663/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 661,
+							"h": 661,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2812814696,
+			"id_str": "2812814696",
+			"name": "Sheffield Philosophy",
+			"screen_name": "Sheffphilosophy",
+			"location": "45 Victoria Street, Sheffield",
+			"description": "Dept. of Philosophy at the University of Sheffield. Top two in UK in REF 2014 for quality of publications and internationally excellent research.",
+			"url": "http://t.co/rS43g4UbLN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/rS43g4UbLN",
+							"expanded_url": "http://www.sheffield.ac.uk/philosophy",
+							"display_url": "sheffield.ac.uk/philosophy",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1913,
+			"friends_count": 501,
+			"listed_count": 50,
+			"created_at": "Tue Sep 16 09:48:37 +0000 2014",
+			"favourites_count": 315,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4089,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1196465304848781312/OO-hwTa0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1196465304848781312/OO-hwTa0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2812814696/1566568524",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our very own Professor Rosanna Keefe, is back from Spain, where she was a keynote speaker at the Conference of the Spanish Society for Analytic Philosophy in Valencia! 🇪🇸 #IXSEFAConference https://t.co/6CqAU4D9ST"
+	},
+	{
 		"created_at": "Mon Dec 02 13:57:40 +0000 2019",
 		"id": 1201500682232238000,
 		"id_str": "1201500682232238081",
