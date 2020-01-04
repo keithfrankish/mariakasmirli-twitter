5728e96f7f6a0fea2af05605c7a3b7e962b25666
Keith Frankish <k.frankish@gmail.com>
2020-01-04 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2019,
-	"created_at": "Wed Jan 01 00:11:44 +0000 2020",
+	"tweets": 2020,
+	"created_at": "Sat Jan 04 00:11:44 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,140 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Fri Jan 03 14:37:59 +0000 2020",
+		"id": 1213105197733601300,
+		"id_str": "1213107242515521537",
+		"full_text": "\"Kids who have less, need more\": The fight over school funding https://t.co/8iMH2JeZ5X #education #edchat #teaching #teacher",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "education",
+					"indices": [
+						87,
+						97
+					]
+				},
+				{
+					"text": "edchat",
+					"indices": [
+						98,
+						105
+					]
+				},
+				{
+					"text": "teaching",
+					"indices": [
+						106,
+						115
+					]
+				},
+				{
+					"text": "teacher",
+					"indices": [
+						116,
+						124
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8iMH2JeZ5X",
+					"expanded_url": "https://hechingerreport.org/kids-who-have-less-need-more-the-fight-over-school-funding/",
+					"display_url": "hechingerreport.org/kids-who-have-…",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tweepsmap.com\" rel=\"nofollow\">Tweepsmap</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2393313872,
+			"id_str": "2393313872",
+			"name": "Matt Hiefield",
+			"screen_name": "MattHiefield",
+			"location": "Beaverton, OR",
+			"description": "HS teacher for 25 yrs. Peace Corps. Future Ready/Google/Apple Exploring #digitalequity, #homeworkgap & #digitaldivide Issues!   Hablo español, je parle français",
+			"url": "https://t.co/b3oEF13h37",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b3oEF13h37",
+							"expanded_url": "http://digitaldivideinuseducation.blogspot.com/",
+							"display_url": "…gitaldivideinuseducation.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42556,
+			"friends_count": 33700,
+			"listed_count": 414,
+			"created_at": "Sun Mar 16 21:44:34 +0000 2014",
+			"favourites_count": 2237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8138,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2393313872/1529588892",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 16,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Kids who have less, need more\": The fight over school funding https://t.co/8iMH2JeZ5X #education #edchat #teaching #teacher"
+	}
+]
