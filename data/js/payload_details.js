6f59777b6dec507661b7feca249a85ab085bd5d2
Keith Frankish <k.frankish@gmail.com>
2020-01-07 02:11:45 +0200

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2021,
-	"created_at": "Sun Jan 05 00:11:44 +0000 2020",
+	"tweets": 2024,
+	"created_at": "Tue Jan 07 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,487 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 06 16:43:07 +0000 2020",
+		"id": 1214219787959709700,
+		"id_str": "1214225896338526209",
+		"full_text": "This is an amazing visual for language acquisition!  Thanks to beavertonsd's multilingual dept. for creating principles that can apply to any classroom.  https://t.co/ONR9Om3xZA #ell #ellchat  #teaching #edchat",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ell",
+					"indices": [
+						178,
+						182
+					]
+				},
+				{
+					"text": "ellchat",
+					"indices": [
+						183,
+						191
+					]
+				},
+				{
+					"text": "teaching",
+					"indices": [
+						193,
+						202
+					]
+				},
+				{
+					"text": "edchat",
+					"indices": [
+						203,
+						210
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ONR9Om3xZA",
+					"expanded_url": "http://bit.ly/2nUwpOv",
+					"display_url": "bit.ly/2nUwpOv",
+					"indices": [
+						154,
+						177
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tweepsmap.com\" rel=\"nofollow\">Tweepsmap</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2393313872,
+			"id_str": "2393313872",
+			"name": "Matt Hiefield",
+			"screen_name": "MattHiefield",
+			"location": "Beaverton, OR",
+			"description": "HS teacher for 25 yrs. Peace Corps. Future Ready/Google/Apple Exploring #digitalequity, #homeworkgap & #digitaldivide Issues!   Hablo español, je parle français",
+			"url": "https://t.co/b3oEF13h37",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b3oEF13h37",
+							"expanded_url": "http://digitaldivideinuseducation.blogspot.com/",
+							"display_url": "…gitaldivideinuseducation.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42562,
+			"friends_count": 33712,
+			"listed_count": 413,
+			"created_at": "Sun Mar 16 21:44:34 +0000 2014",
+			"favourites_count": 2243,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2393313872/1529588892",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is an amazing visual for language acquisition!  Thanks to beavertonsd's multilingual dept. for creating principles that can apply to any classroom.  https://t.co/ONR9Om3xZA #ell #ellchat  #teaching #edchat"
+	},
+	{
+		"created_at": "Mon Jan 06 16:22:53 +0000 2020",
+		"id": 1214185680777220000,
+		"id_str": "1214220805946691587",
+		"full_text": "I will be delivering a 10-week philosophy course for teenagers at @WaterstonesLPL from Saturday 1st Feb (10-11am). Please share widely. E-mail philosophyforteens@hotmail.co.uk to reserve a place. https://t.co/HkoOgLHDRP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WaterstonesLPL",
+					"name": "Waterstones Liverpool",
+					"id": 20266400,
+					"id_str": "20266400",
+					"indices": [
+						66,
+						81
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1214185648938029000,
+					"id_str": "1214185648938029057",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/ENmnaihUEAEtEhP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENmnaihUEAEtEhP.jpg",
+					"url": "https://t.co/HkoOgLHDRP",
+					"display_url": "pic.twitter.com/HkoOgLHDRP",
+					"expanded_url": "https://twitter.com/RoyalInstPhilos/status/1214185680777220097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 563,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 993,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1173,
+							"h": 1417,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1214185648938029000,
+					"id_str": "1214185648938029057",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/ENmnaihUEAEtEhP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENmnaihUEAEtEhP.jpg",
+					"url": "https://t.co/HkoOgLHDRP",
+					"display_url": "pic.twitter.com/HkoOgLHDRP",
+					"expanded_url": "https://twitter.com/RoyalInstPhilos/status/1214185680777220097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 563,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 993,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1173,
+							"h": 1417,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy 4 Schools",
+			"screen_name": "RoyalInstPhilos",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: delivering funded 'intro to philosophy' courses in schools/colleges on behalf of the Royal Institute of Philosophy. P4C Facilitator L3 SAPERE",
+			"url": "https://t.co/P6uOGjR0rz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/P6uOGjR0rz",
+							"expanded_url": "http://royalinstitutephilosophy.org/funding-education/philosophy-in-schools/",
+							"display_url": "royalinstitutephilosophy.org/funding-educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2543,
+			"friends_count": 5006,
+			"listed_count": 38,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 488,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1790,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 27,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I will be delivering a 10-week philosophy course for teenagers at @WaterstonesLPL from Saturday 1st Feb (10-11am). Please share widely. E-mail philosophyforteens@hotmail.co.uk to reserve a place. https://t.co/HkoOgLHDRP"
+	},
+	{
+		"created_at": "Mon Jan 06 16:22:00 +0000 2020",
+		"id": 1214214764500603000,
+		"id_str": "1214220580356083712",
+		"full_text": "In June, ⁦@ElenaRoccaPD⁩ and I will teach a new course on interdisciplinarity and expert disagreement in sustainability research at ⁦@UniNMBU⁩! It’s a pilot so we hope we get lots of students signing up.  https://t.co/1tbQzW84dD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ElenaRoccaPD",
+					"name": "Elena Rocca",
+					"id": 4122171795,
+					"id_str": "4122171795",
+					"indices": [
+						10,
+						23
+					]
+				},
+				{
+					"screen_name": "UniNMBU",
+					"name": "NMBU",
+					"id": 1878721232,
+					"id_str": "1878721232",
+					"indices": [
+						133,
+						141
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1tbQzW84dD",
+					"expanded_url": "https://www.nmbu.no/en/faculty/hh/research/centers/caps/news/node/39198",
+					"display_url": "nmbu.no/en/faculty/hh/…",
+					"indices": [
+						205,
+						228
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 75838766,
+			"id_str": "75838766",
+			"name": "Dr Rani Lill Anjum",
+			"screen_name": "ranilillanjum",
+			"location": "Ås, Norge",
+			"description": "Philosopher and co-Director of @UniNMBU Centre for Applied Philosophy of Science. PI of @Cause_Health. I work on causation & philosophical bias in science",
+			"url": "https://t.co/KrH2BLvIbB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KrH2BLvIbB",
+							"expanded_url": "https://ranilillanjum.wordpress.com",
+							"display_url": "ranilillanjum.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11078,
+			"friends_count": 4039,
+			"listed_count": 478,
+			"created_at": "Sun Sep 20 17:59:42 +0000 2009",
+			"favourites_count": 3913,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 76744,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1151232754476097537/bdgGLboA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1151232754476097537/bdgGLboA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/75838766/1570735201",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 18,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In June, ⁦@ElenaRoccaPD⁩ and I will teach a new course on interdisciplinarity and expert disagreement in sustainability research at ⁦@UniNMBU⁩! It’s a pilot so we hope we get lots of students signing up.  https://t.co/1tbQzW84dD"
+	},
+	{
 		"created_at": "Sat Jan 04 07:30:03 +0000 2020",
 		"id": 1213359843978952700,
 		"id_str": "1213361936768929795",
