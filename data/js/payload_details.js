0385d96a1d0a5beff1b631894edd84ef46d22373
Keith Frankish <k.frankish@gmail.com>
2020-01-08 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2024,
-	"created_at": "Tue Jan 07 00:11:44 +0000 2020",
+	"tweets": 2025,
+	"created_at": "Wed Jan 08 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 07 20:57:35 +0000 2020",
+		"id": 1033379003346776000,
+		"id_str": "1214652324108476416",
+		"full_text": "https://t.co/e6gYqHBog0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1033376480238100500,
+					"id_str": "1033376480238100482",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1033376480238100482/pu/img/6lYoTgvQYmLJG647.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1033376480238100482/pu/img/6lYoTgvQYmLJG647.jpg",
+					"url": "https://t.co/e6gYqHBog0",
+					"display_url": "pic.twitter.com/e6gYqHBog0",
+					"expanded_url": "https://twitter.com/MattHiefield/status/1033379003346776064/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1033376480238100500,
+					"id_str": "1033376480238100482",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1033376480238100482/pu/img/6lYoTgvQYmLJG647.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1033376480238100482/pu/img/6lYoTgvQYmLJG647.jpg",
+					"url": "https://t.co/e6gYqHBog0",
+					"display_url": "pic.twitter.com/e6gYqHBog0",
+					"expanded_url": "https://twitter.com/MattHiefield/status/1033379003346776064/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 72766,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1033376480238100482/pu/vid/320x180/-kN0EoNmXrTgF4Ys.mp4?tag=5"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1033376480238100482/pu/vid/640x360/bYGx0LrvVbsf6H-W.mp4?tag=5"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1033376480238100482/pu/vid/1280x720/idRobXVq8eZb_ZV3.mp4?tag=5"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1033376480238100482/pu/pl/WbrzliX5eEHj9qff.m3u8?tag=5"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2393313872,
+			"id_str": "2393313872",
+			"name": "Matt Hiefield",
+			"screen_name": "MattHiefield",
+			"location": "Beaverton, OR",
+			"description": "HS teacher for 25 yrs. Peace Corps. Future Ready/Google/Apple Exploring #digitalequity, #homeworkgap & #digitaldivide Issues!   Hablo español, je parle français",
+			"url": "https://t.co/b3oEF13h37",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b3oEF13h37",
+							"expanded_url": "http://digitaldivideinuseducation.blogspot.com/",
+							"display_url": "…gitaldivideinuseducation.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42588,
+			"friends_count": 33706,
+			"listed_count": 414,
+			"created_at": "Sun Mar 16 21:44:34 +0000 2014",
+			"favourites_count": 2243,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8158,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/846059128212484096/M7f9qmKu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2393313872/1529588892",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 248,
+		"favorite_count": 551,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/e6gYqHBog0"
+	},
+	{
 		"created_at": "Mon Jan 06 16:43:07 +0000 2020",
 		"id": 1214219787959709700,
 		"id_str": "1214225896338526209",
