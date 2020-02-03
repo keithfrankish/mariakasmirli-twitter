9b7ca808e411d83f68b56b2d484426221beb248c
Keith Frankish <k.frankish@gmail.com>
2020-02-03 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2034,
-	"created_at": "Sat Feb 01 00:11:44 +0000 2020",
+	"tweets": 2035,
+	"created_at": "Mon Feb 03 00:11:44 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,115 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sun Feb 02 12:19:35 +0000 2020",
+		"id": 1223902574241140700,
+		"id_str": "1223944046684581892",
+		"full_text": "Rejection is exceedingly common in academia. The acceptance rate for top journals is about 10%. Can you imagine a lawyer who loses 90% of his cases or a doctor who loses 90% of her patients?\n\n#AcademicChatter #AcademicTwitter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			225
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AcademicChatter",
+					"indices": [
+						192,
+						208
+					]
+				},
+				{
+					"text": "AcademicTwitter",
+					"indices": [
+						209,
+						225
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 908226487928594400,
+			"id_str": "908226487928594432",
+			"name": "Prof. Andrew R. Timming",
+			"screen_name": "timminglab",
+			"location": "Perth, Western Australia",
+			"description": "Social science laboratory focusing on human resource management research. I make general observations on Twitter that do not pertain to any one institution.",
+			"url": "https://t.co/jpnsctyj4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jpnsctyj4F",
+							"expanded_url": "http://www.timminglab.com",
+							"display_url": "timminglab.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15022,
+			"friends_count": 13169,
+			"listed_count": 63,
+			"created_at": "Thu Sep 14 07:10:43 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1696,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/908226487928594432/1560415816",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 207,
+		"favorite_count": 1256,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Rejection is exceedingly common in academia. The acceptance rate for top journals is about 10%. Can you imagine a lawyer who loses 90% of his cases or a doctor who loses 90% of her patients?\n\n#AcademicChatter #AcademicTwitter"
+	}
+]
