baee9c9554c8735d2d7d16166fff4a3f86e627e8
Keith Frankish <k.frankish@gmail.com>
2020-02-11 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2035,
-	"created_at": "Mon Feb 03 00:11:44 +0000 2020",
+	"tweets": 2036,
+	"created_at": "Tue Feb 11 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 15:39:12 +0000 2020",
+		"id": 1226892655885529000,
+		"id_str": "1226893385258151938",
+		"full_text": "I'll be giving a lecture in Athens tomorrow evening at ΕΚΠΑ, ΙΦΕ. --&gt;  Lecture, Tuesday, February the 11th (2020), Propylaia (amphitheater 'Drakopoulos'), 19:00-21:00. Hope to meet some of you there! https://t.co/vAvcthHWKY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1226892611090292700,
+					"id_str": "1226892611090292738",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/EQbMVFWWoAIDGwW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQbMVFWWoAIDGwW.jpg",
+					"url": "https://t.co/vAvcthHWKY",
+					"display_url": "pic.twitter.com/vAvcthHWKY",
+					"expanded_url": "https://twitter.com/keithfrankish/status/1226892655885529088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1289,
+							"h": 967,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1226892611090292700,
+					"id_str": "1226892611090292738",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/EQbMVFWWoAIDGwW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQbMVFWWoAIDGwW.jpg",
+					"url": "https://t.co/vAvcthHWKY",
+					"display_url": "pic.twitter.com/vAvcthHWKY",
+					"expanded_url": "https://twitter.com/keithfrankish/status/1226892655885529088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1289,
+							"h": 967,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher & writer. European. Honorary Reader @sheffielduni. Interests in consciousness, reasoning, belief. Tweeting phil mind, cog sci, Greece, links & chat.",
+			"url": "http://t.co/tXhT44huzC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tXhT44huzC",
+							"expanded_url": "http://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11926,
+			"friends_count": 2016,
+			"listed_count": 514,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 33930,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 55278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1554843010",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 43,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'll be giving a lecture in Athens tomorrow evening at ΕΚΠΑ, ΙΦΕ. --&gt;  Lecture, Tuesday, February the 11th (2020), Propylaia (amphitheater 'Drakopoulos'), 19:00-21:00. Hope to meet some of you there! https://t.co/vAvcthHWKY"
+	},
+	{
 		"created_at": "Sun Feb 02 12:19:35 +0000 2020",
 		"id": 1223902574241140700,
 		"id_str": "1223944046684581892",
