751438a429e82cb17d0eb19ee32c21e4ab02c9f7
Keith Frankish <k.frankish@gmail.com>
2020-02-14 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2036,
-	"created_at": "Tue Feb 11 00:11:44 +0000 2020",
+	"tweets": 2037,
+	"created_at": "Fri Feb 14 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 13 08:04:03 +0000 2020",
+		"id": 1227696010509639700,
+		"id_str": "1227866007521370113",
+		"full_text": "Today is International Day for Women and Girls in Science . Finland could do much better! https://t.co/Oxv2YDUMoZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1227696003928723500,
+					"id_str": "1227696003928723458",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EQmnAs5WkAIQ4Eg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQmnAs5WkAIQ4Eg.jpg",
+					"url": "https://t.co/Oxv2YDUMoZ",
+					"display_url": "pic.twitter.com/Oxv2YDUMoZ",
+					"expanded_url": "https://twitter.com/Kari_Kivinen/status/1227696010509639681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1653,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 444,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 784,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227696003928723500,
+					"id_str": "1227696003928723458",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EQmnAs5WkAIQ4Eg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQmnAs5WkAIQ4Eg.jpg",
+					"url": "https://t.co/Oxv2YDUMoZ",
+					"display_url": "pic.twitter.com/Oxv2YDUMoZ",
+					"expanded_url": "https://twitter.com/Kari_Kivinen/status/1227696010509639681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1653,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 444,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 784,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 77735405,
+			"id_str": "77735405",
+			"name": "Kari Kivinen",
+			"screen_name": "Kari_Kivinen",
+			"location": "Helsinki, Suomi",
+			"description": "Interested in media & information literacy, fact-checking, motivation & self-regulation research, multilingualism, IP-education, language teaching, French, etc.",
+			"url": "https://t.co/sKuluJBIXh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sKuluJBIXh",
+							"expanded_url": "http://kivinen.wordpress.com",
+							"display_url": "kivinen.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 771,
+			"friends_count": 1097,
+			"listed_count": 26,
+			"created_at": "Sun Sep 27 13:29:13 +0000 2009",
+			"favourites_count": 9204,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4073,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102121621995827200/Mk7DxwdQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102121621995827200/Mk7DxwdQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/77735405/1551601330",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "5ef832bb704339b0",
+			"url": "https://api.twitter.com/1.1/geo/id/5ef832bb704339b0.json",
+			"place_type": "city",
+			"name": "Helsinki",
+			"full_name": "Helsinki, Finland",
+			"country_code": "FI",
+			"country": "Finland",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							24.78281,
+							60.021032
+						],
+						[
+							25.2544364,
+							60.021032
+						],
+						[
+							25.2544364,
+							60.2979104
+						],
+						[
+							24.78281,
+							60.2979104
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is International Day for Women and Girls in Science . Finland could do much better! https://t.co/Oxv2YDUMoZ"
+	},
+	{
 		"created_at": "Mon Feb 10 15:39:12 +0000 2020",
 		"id": 1226892655885529000,
 		"id_str": "1226893385258151938",
