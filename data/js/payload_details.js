b6af75526ad07136d8fa8cd47fc1d278f04ef2f9
Keith Frankish <k.frankish@gmail.com>
2020-02-16 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2037,
-	"created_at": "Fri Feb 14 00:11:45 +0000 2020",
+	"tweets": 2038,
+	"created_at": "Sun Feb 16 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 15 14:54:58 +0000 2020",
+		"id": 1228633341387251700,
+		"id_str": "1228694192874020864",
+		"full_text": "Great turnout - despite the weather - at @Sheffphilosophy's open day this morning! Looking forward to chatting with the next generation of Sheffield philosophers in the coffee break.\n\n@sheffielduni @UniShefAH https://t.co/kT6uLP28iZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Sheffphilosophy",
+					"name": "Sheffield Philosophy",
+					"id": 2812814696,
+					"id_str": "2812814696",
+					"indices": [
+						41,
+						57
+					]
+				},
+				{
+					"screen_name": "sheffielduni",
+					"name": "The University of Sheffield",
+					"id": 19341622,
+					"id_str": "19341622",
+					"indices": [
+						184,
+						197
+					]
+				},
+				{
+					"screen_name": "UniShefAH",
+					"name": "Faculty of Arts and Humanities",
+					"id": 3235804654,
+					"id_str": "3235804654",
+					"indices": [
+						198,
+						208
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1228633331731980300,
+					"id_str": "1228633331731980291",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/EQz7gXEW4AMM5gj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQz7gXEW4AMM5gj.jpg",
+					"url": "https://t.co/kT6uLP28iZ",
+					"display_url": "pic.twitter.com/kT6uLP28iZ",
+					"expanded_url": "https://twitter.com/JoshForstenzer/status/1228633341387251712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1228633331731980300,
+					"id_str": "1228633331731980291",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/EQz7gXEW4AMM5gj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQz7gXEW4AMM5gj.jpg",
+					"url": "https://t.co/kT6uLP28iZ",
+					"display_url": "pic.twitter.com/kT6uLP28iZ",
+					"expanded_url": "https://twitter.com/JoshForstenzer/status/1228633341387251712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 166988769,
+			"id_str": "166988769",
+			"name": "Joshua Forstenzer",
+			"screen_name": "JoshForstenzer",
+			"location": "Sheffield, England",
+			"description": "Culturally confused French-American philosopher. Dewey scholar. Co-director of the Centre for Engaged Philosophy. All views my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 666,
+			"friends_count": 701,
+			"listed_count": 14,
+			"created_at": "Thu Jul 15 13:50:08 +0000 2010",
+			"favourites_count": 3387,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1060,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/857963777077256192/_zwuGBPM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/857963777077256192/_zwuGBPM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/166988769/1493391314",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great turnout - despite the weather - at @Sheffphilosophy's open day this morning! Looking forward to chatting with the next generation of Sheffield philosophers in the coffee break.\n\n@sheffielduni @UniShefAH https://t.co/kT6uLP28iZ"
+	},
+	{
 		"created_at": "Thu Feb 13 08:04:03 +0000 2020",
 		"id": 1227696010509639700,
 		"id_str": "1227866007521370113",
