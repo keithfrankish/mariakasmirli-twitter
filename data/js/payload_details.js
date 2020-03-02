0cadccda09eff4f5443b2f9442aa9dcca0ad65b9
Keith Frankish <k.frankish@gmail.com>
2020-03-02 02:11:45 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2044,
-	"created_at": "Thu Feb 27 00:11:45 +0000 2020",
+	"tweets": 2046,
+	"created_at": "Mon Mar 02 00:11:44 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_03.js
@@ -0,0 +1,269 @@
+Grailbird.data.tweets_2020_03 = 
+[
+	{
+		"created_at": "Sun Mar 01 15:12:22 +0000 2020",
+		"id": 1234134390785093600,
+		"id_str": "1234134390785093632",
+		"full_text": "The Coronavirus Is Much Worse Than You Think | Psychology Today https://t.co/bcjOY5KmyS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bcjOY5KmyS",
+					"expanded_url": "https://www.psychologytoday.com/blog/culture-mind-and-brain/202002/the-coronavirus-is-much-worse-you-think",
+					"display_url": "psychologytoday.com/blog/culture-m…",
+					"indices": [
+						64,
+						87
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 728,
+			"friends_count": 522,
+			"listed_count": 35,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 3217,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1705,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1208132236261937152/UiKHzYd5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1208132236261937152/UiKHzYd5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1576876149",
+			"profile_link_color": "22262A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Coronavirus Is Much Worse Than You Think | Psychology Today https://t.co/bcjOY5KmyS"
+	},
+	{
+		"created_at": "Sun Mar 01 10:49:45 +0000 2020",
+		"id": 1233073486236651500,
+		"id_str": "1234068300650426370",
+		"full_text": "A certain PhD supervisor who shall remain nameless said to me yday that Wittgensteinians are sometimes more like religious disciples than philosophical followers and I was like wow that’s rude, but it’s barely 24 hours later and I’ve already accidentally proven him right ;( https://t.co/3X0M0SunYq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1233073479760609300,
+					"id_str": "1233073479760609283",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/ERzBy_cWAAM1mxN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERzBy_cWAAM1mxN.jpg",
+					"url": "https://t.co/3X0M0SunYq",
+					"display_url": "pic.twitter.com/3X0M0SunYq",
+					"expanded_url": "https://twitter.com/philosophilith/status/1233073486236651520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 539,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1622,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1233073479760609300,
+					"id_str": "1233073479760609283",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/ERzBy_cWAAM1mxN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERzBy_cWAAM1mxN.jpg",
+					"url": "https://t.co/3X0M0SunYq",
+					"display_url": "pic.twitter.com/3X0M0SunYq",
+					"expanded_url": "https://twitter.com/philosophilith/status/1233073486236651520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 539,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1622,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1108358830105206800,
+			"id_str": "1108358830105206784",
+			"name": "Lilith",
+			"screen_name": "philosophilith",
+			"location": "",
+			"description": "Philosophy PhD student at the University of Edinburgh, working on this n that",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 157,
+			"friends_count": 102,
+			"listed_count": 1,
+			"created_at": "Wed Mar 20 13:25:12 +0000 2019",
+			"favourites_count": 202,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 148,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1108359194120503296/ny0RzPnC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108359194120503296/ny0RzPnC_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 66,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A certain PhD supervisor who shall remain nameless said to me yday that Wittgensteinians are sometimes more like religious disciples than philosophical followers and I was like wow that’s rude, but it’s barely 24 hours later and I’ve already accidentally proven him right ;( https://t.co/3X0M0SunYq"
+	}
+]
