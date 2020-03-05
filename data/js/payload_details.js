69fcda824fb2a5f3d541fb705562a19d191ed001
Keith Frankish <k.frankish@gmail.com>
2020-03-05 02:11:45 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2046,
-	"created_at": "Mon Mar 02 00:11:44 +0000 2020",
+	"tweets": 2048,
+	"created_at": "Thu Mar 05 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,370 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 04 19:48:30 +0000 2020",
+		"id": 1235213927082057700,
+		"id_str": "1235291045216292867",
+		"full_text": "Don’t miss two world experts on Isaiah Berlin discussing his thought @blackwelloxford on Saturday morning. Free event, all welcome. I’ll be chairing this. https://t.co/BM3vrYHVu5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "blackwelloxford",
+					"name": "Blackwell's Oxford",
+					"id": 119073070,
+					"id_str": "119073070",
+					"indices": [
+						69,
+						85
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BM3vrYHVu5",
+					"expanded_url": "https://twitter.com/blackwelloxford/status/1235186480345051136",
+					"display_url": "twitter.com/blackwelloxfor…",
+					"indices": [
+						155,
+						178
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37238862,
+			"id_str": "37238862",
+			"name": "Nigel Warburton",
+			"screen_name": "philosophybites",
+			"location": "Oxford  & London UK",
+			"description": "A Little History of Philosophy, Philosophy Bites podcast. Consultant editor @aeonmag, @thenewphil, @five_books. Founding faculty @weareLIS. @unitedagents",
+			"url": "https://t.co/kD7ridjs8Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kD7ridjs8Q",
+							"expanded_url": "http://www.nigelwarburton.com",
+							"display_url": "nigelwarburton.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 80249,
+			"friends_count": 6998,
+			"listed_count": 2571,
+			"created_at": "Sat May 02 17:20:21 +0000 2009",
+			"favourites_count": 44970,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 47715,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B5B8B4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37238862/1398336718",
+			"profile_link_color": "000DFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1235186480345051100,
+		"quoted_status_id_str": "1235186480345051136",
+		"quoted_status_permalink": {
+			"url": "https://t.co/BM3vrYHVu5",
+			"expanded": "https://twitter.com/blackwelloxford/status/1235186480345051136",
+			"display": "twitter.com/blackwelloxfor…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 04 12:53:00 +0000 2020",
+			"id": 1235186480345051100,
+			"id_str": "1235186480345051136",
+			"full_text": "The next #PhilosophyInTheBookshop is happening this Saturday (7/3) at 11am.\n\n@philosophybites will be discussing the philosophy of Isaiah Berlin with @hardywolfson and Johnny Lyons.\n\nThis event is free to attend!\n\nhttps://t.co/uJitaf9XeR",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				237
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "PhilosophyInTheBookshop",
+						"indices": [
+							9,
+							33
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "philosophybites",
+						"name": "Nigel Warburton",
+						"id": 37238862,
+						"id_str": "37238862",
+						"indices": [
+							77,
+							93
+						]
+					},
+					{
+						"screen_name": "hardywolfson",
+						"name": "Henry Hardy",
+						"id": 87793197,
+						"id_str": "87793197",
+						"indices": [
+							150,
+							163
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/uJitaf9XeR",
+						"expanded_url": "https://www.eventbrite.co.uk/e/philosophy-in-the-bookshop-henry-hardy-and-johnny-lyons-tickets-90944091225",
+						"display_url": "eventbrite.co.uk/e/philosophy-i…",
+						"indices": [
+							214,
+							237
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 119073070,
+				"id_str": "119073070",
+				"name": "Blackwell's Oxford",
+				"screen_name": "blackwelloxford",
+				"location": "Oxford, England",
+				"description": "Blackwell's in Oxford has been serving readers since 1879 and is one of the most famous bookshops in the world. Bookshops on Broad Street and at Westgate.",
+				"url": "https://t.co/Quox7njEj2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Quox7njEj2",
+								"expanded_url": "http://blackwell.co.uk/oxford",
+								"display_url": "blackwell.co.uk/oxford",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 26010,
+				"friends_count": 3729,
+				"listed_count": 484,
+				"created_at": "Tue Mar 02 15:53:43 +0000 2010",
+				"favourites_count": 8285,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 31990,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "002F5E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1058051439111667713/btzKfd6-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058051439111667713/btzKfd6-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/119073070/1547556273",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 6,
+		"favorite_count": 17,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Don’t miss two world experts on Isaiah Berlin discussing his thought @blackwelloxford on Saturday morning. Free event, all welcome. I’ll be chairing this. https://t.co/BM3vrYHVu5"
+	},
+	{
+		"created_at": "Wed Mar 04 13:25:24 +0000 2020",
+		"id": 1235194635988996000,
+		"id_str": "1235194635988996096",
+		"full_text": "@elskivi ... and that's how real progress is made! :)",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "elskivi",
+					"name": "Elsa Kivinen 🐟",
+					"id": 813610931129380900,
+					"id_str": "813610931129380864",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1234884414783524900,
+		"in_reply_to_status_id_str": "1234884414783524866",
+		"in_reply_to_user_id": 813610931129380900,
+		"in_reply_to_user_id_str": "813610931129380864",
+		"in_reply_to_screen_name": "elskivi",
+		"user": {
+			"id": 103135865,
+			"id_str": "103135865",
+			"name": "Maria Kasmirli",
+			"screen_name": "mariakasmirli",
+			"location": "Heraklion, Crete, Greece",
+			"description": "British/Greek philosopher and teacher. Tweeting about philosophy, education, and P4C.",
+			"url": "http://t.co/lAYE8xCn3R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/lAYE8xCn3R",
+							"expanded_url": "http://mariakasmirli.com",
+							"display_url": "mariakasmirli.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 727,
+			"friends_count": 521,
+			"listed_count": 35,
+			"created_at": "Sat Jan 09 01:01:31 +0000 2010",
+			"favourites_count": 3222,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "24476A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1208132236261937152/UiKHzYd5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1208132236261937152/UiKHzYd5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/103135865/1576876149",
+			"profile_link_color": "22262A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "8C8C89",
+			"profile_text_color": "123133",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@elskivi ... and that's how real progress is made! :)"
+	},
+	{
 		"created_at": "Sun Mar 01 15:12:22 +0000 2020",
 		"id": 1234134390785093600,
 		"id_str": "1234134390785093632",
