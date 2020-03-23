ecd300e386cfd057c69bbce476ea7af9cb119f73
Keith Frankish <k.frankish@gmail.com>
2020-03-23 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2062,
-	"created_at": "Sun Mar 22 00:11:45 +0000 2020",
+	"tweets": 2063,
+	"created_at": "Mon Mar 23 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 22 18:26:55 +0000 2020",
+		"id": 1241683946611650600,
+		"id_str": "1241793497159647233",
+		"full_text": "Can I please remind you to check in regularly on your PhD students via phone or email. They are vulnerable. Many (perhaps most) live alone with no family nearby. We supervisors are the closest they have to a family.\n\n#AcademicTwitter \n#AcademicChatter \n#Coronavirus",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AcademicTwitter",
+					"indices": [
+						217,
+						233
+					]
+				},
+				{
+					"text": "AcademicChatter",
+					"indices": [
+						235,
+						251
+					]
+				},
+				{
+					"text": "Coronavirus",
+					"indices": [
+						253,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 908226487928594400,
+			"id_str": "908226487928594432",
+			"name": "Prof. Andrew R. Timming",
+			"screen_name": "timminglab",
+			"location": "Perth, Western Australia",
+			"description": "Social science laboratory focusing on human resource management research. I make general observations on Twitter that do not pertain to any one institution.",
+			"url": "https://t.co/jpnsctyj4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jpnsctyj4F",
+							"expanded_url": "http://www.timminglab.com",
+							"display_url": "timminglab.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16337,
+			"friends_count": 10421,
+			"listed_count": 70,
+			"created_at": "Thu Sep 14 07:10:43 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1412,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/908226487928594432/1560415816",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 611,
+		"favorite_count": 3723,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Can I please remind you to check in regularly on your PhD students via phone or email. They are vulnerable. Many (perhaps most) live alone with no family nearby. We supervisors are the closest they have to a family.\n\n#AcademicTwitter \n#AcademicChatter \n#Coronavirus"
+	},
+	{
 		"created_at": "Sat Mar 21 19:46:22 +0000 2020",
 		"id": 1241417361883893800,
 		"id_str": "1241451102144466944",
