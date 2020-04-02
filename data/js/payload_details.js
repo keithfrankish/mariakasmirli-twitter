1be303c25629a6bad51e69e791360b7a177f7912
Keith Frankish <k.frankish@gmail.com>
2020-04-02 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2068,
-	"created_at": "Sat Mar 28 00:11:45 +0000 2020",
+	"tweets": 2069,
+	"created_at": "Thu Apr 02 00:11:44 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,96 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 06:40:19 +0000 2020",
+		"id": 1245120112165863400,
+		"id_str": "1245239553125908487",
+		"full_text": "https://t.co/bBURBfsmv8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bBURBfsmv8",
+					"expanded_url": "https://brocku.ca/brock-news/2020/03/brock-university-donates-truck-full-of-ppe-to-niagara-hospitals/",
+					"display_url": "brocku.ca/brock-news/202…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 970766939009306600,
+			"id_str": "970766939009306625",
+			"name": "Tim Kenyon",
+			"screen_name": "TimAKenyon",
+			"location": "",
+			"description": "Human person, research admin, critical thinking author, phil. of language, social epistemology, cycling. Tweets *at most* my own views, frequently less. he",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 442,
+			"friends_count": 542,
+			"listed_count": 7,
+			"created_at": "Mon Mar 05 21:04:09 +0000 2018",
+			"favourites_count": 5594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1392,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/979302327545925632/n8-CCycX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/979302327545925632/n8-CCycX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/970766939009306625/1520811655",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 35,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/bBURBfsmv8"
+	}
+]
