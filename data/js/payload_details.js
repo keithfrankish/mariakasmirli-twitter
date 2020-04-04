05e6b60da70ec742d7fa177ed0e3eaf498a582fd
Keith Frankish <k.frankish@gmail.com>
2020-04-04 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2069,
-	"created_at": "Thu Apr 02 00:11:44 +0000 2020",
+	"tweets": 2070,
+	"created_at": "Sat Apr 04 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 03 08:21:03 +0000 2020",
+		"id": 1245988493794455600,
+		"id_str": "1245989677917749259",
+		"full_text": "Please respond to this consultation to share your views and experience of Unequal impact of #Coronavirus #Covid-19 on #disabled people. Open for evidence until Thursday 30 April 2020 https://t.co/2AzkKXrZCY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			206
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Coronavirus",
+					"indices": [
+						92,
+						104
+					]
+				},
+				{
+					"text": "Covid",
+					"indices": [
+						105,
+						111
+					]
+				},
+				{
+					"text": "disabled",
+					"indices": [
+						118,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2AzkKXrZCY",
+					"expanded_url": "https://committees.parliament.uk/work/227/unequal-impact-coronavirus-covid19-and-the-impact-on-people-with-protected-characteristics",
+					"display_url": "committees.parliament.uk/work/227/unequ…",
+					"indices": [
+						183,
+						206
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2906241221,
+			"id_str": "2906241221",
+			"name": "Disability&Society",
+			"screen_name": "JDisSoc",
+			"location": "",
+			"description": "The official account of the journal Disability & Society  - publishing a wide range of perspectives including the voices of disabled people.",
+			"url": "http://t.co/6LlAIKBWFL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6LlAIKBWFL",
+							"expanded_url": "http://www.tandfonline.com/toc/cdso20/current#.VIGZwbdF270",
+							"display_url": "tandfonline.com/toc/cdso20/cur…",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10338,
+			"friends_count": 6447,
+			"listed_count": 174,
+			"created_at": "Fri Dec 05 11:18:11 +0000 2014",
+			"favourites_count": 4925,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7767,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/962365524876382208/daqoElLg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/962365524876382208/daqoElLg_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 12,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Please respond to this consultation to share your views and experience of Unequal impact of #Coronavirus #Covid-19 on #disabled people. Open for evidence until Thursday 30 April 2020 https://t.co/2AzkKXrZCY"
+	},
+	{
 		"created_at": "Wed Apr 01 06:40:19 +0000 2020",
 		"id": 1245120112165863400,
 		"id_str": "1245239553125908487",
