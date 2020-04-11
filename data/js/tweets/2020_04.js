0d78dc4e466c97138235aa5fd00835853e63411a
Keith Frankish <k.frankish@gmail.com>
2020-04-11 03:11:46 +0300

    Added 1 tweets

--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 10 15:56:40 +0000 2020",
+		"id": 1248638926258135000,
+		"id_str": "1248641053508481027",
+		"full_text": "'Can I sunbathe in the park?' Mill, Kant and Marx on #LockdownLife  https://t.co/PfXsuIARsP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LockdownLife",
+					"indices": [
+						53,
+						66
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PfXsuIARsP",
+					"expanded_url": "https://www.theguardian.com/commentisfree/2020/apr/10/sunbathing-park-deep-moral-questions-philosophers-coronavirus-individual",
+					"display_url": "theguardian.com/commentisfree/…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy 4 Schools",
+			"screen_name": "RoyalInstPhilos",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: delivering funded 'intro to philosophy' courses in schools/colleges on behalf of the Royal Institute of Philosophy. P4C Facilitator L3 SAPERE",
+			"url": "https://t.co/P6uOGjR0rz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/P6uOGjR0rz",
+							"expanded_url": "http://royalinstitutephilosophy.org/funding-education/philosophy-in-schools/",
+							"display_url": "royalinstitutephilosophy.org/funding-educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2706,
+			"friends_count": 4998,
+			"listed_count": 41,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 543,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1900,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'Can I sunbathe in the park?' Mill, Kant and Marx on #LockdownLife  https://t.co/PfXsuIARsP"
+	},
+	{
 		"created_at": "Thu Apr 09 17:45:15 +0000 2020",
 		"id": 1246137606674530300,
 		"id_str": "1248305989889261573",
