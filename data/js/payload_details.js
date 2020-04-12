273d3b2254ca121306b9059db0f9e9ee51866939
Keith Frankish <k.frankish@gmail.com>
2020-04-12 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2075,
-	"created_at": "Sat Apr 11 00:11:44 +0000 2020",
+	"tweets": 2076,
+	"created_at": "Sun Apr 12 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 11 21:18:13 +0000 2020",
+		"id": 1249057293557280800,
+		"id_str": "1249084363444289536",
+		"full_text": "Κι εμείς είμαστε ευγνώμονες στη Βάσια Πάσχου και όλους της τους συναδέλφους, είτε ιθαγενείς είτε από την Ελλάδα κι αλλού, που εργάζονται ασταμάτητα στο ΕΣΥ μας αυτό τον δύσκολο χρόνο. #πραγματικοί_ήρωες https://t.co/EG28v8ue3q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "πραγματικοί_ήρωες",
+					"indices": [
+						184,
+						202
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EG28v8ue3q",
+					"expanded_url": "https://twitter.com/protothema/status/1249052138828898305",
+					"display_url": "twitter.com/protothema/sta…",
+					"indices": [
+						203,
+						226
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 847892022,
+			"id_str": "847892022",
+			"name": "John Kittmer",
+			"screen_name": "JohnKittmer",
+			"location": "Tooting, London",
+			"description": "Northerner. Ελληνολάτρης. European. Researcher in Greek literature. Chair @AngHellenLeague @GMTru. NED in shipping industry. Interests in LGBT+, ecology, faith.",
+			"url": "https://t.co/gKJm6XXZHH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gKJm6XXZHH",
+							"expanded_url": "https://antapokriseiscorrespondences.blogspot.com",
+							"display_url": "…pokriseiscorrespondences.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5295,
+			"friends_count": 224,
+			"listed_count": 149,
+			"created_at": "Wed Sep 26 18:34:08 +0000 2012",
+			"favourites_count": 9866,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15150,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "98A5AB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234238068309274628/hDNCWP6j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234238068309274628/hDNCWP6j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/847892022/1586039435",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1249052138828898300,
+		"quoted_status_id_str": "1249052138828898305",
+		"quoted_status_permalink": {
+			"url": "https://t.co/EG28v8ue3q",
+			"expanded": "https://twitter.com/protothema/status/1249052138828898305",
+			"display": "twitter.com/protothema/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 11 19:10:10 +0000 2020",
+			"id": 1249052138828898300,
+			"id_str": "1249052138828898305",
+			"full_text": "Κορωνοϊός - Ελληνίδα γιατρός στη Βρετανία: Aυτό είναι το κουρασμένο πρόσωπό μου https://t.co/JZAh47EhIX",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				103
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/JZAh47EhIX",
+						"expanded_url": "https://www.protothema.gr/greece/article/995046/koronoios-ellinida-giatros-sti-vretania-auto-einai-to-kourasmeno-prosopo-mou/?utm_term=Autofeed&utm_medium=Social&utm_source=Twitter#Echobox=1586631919",
+						"display_url": "protothema.gr/greece/article…",
+						"indices": [
+							80,
+							103
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 25584888,
+				"id_str": "25584888",
+				"name": "protothema.gr",
+				"screen_name": "protothema",
+				"location": "Greece, Athens",
+				"description": "Greece’s No. 1 news site & best-selling Sunday newspaper. Follow for breaking news, special reports and more from\n@protothema / #protothema",
+				"url": "http://t.co/vc9gM52lcd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/vc9gM52lcd",
+								"expanded_url": "http://www.protothema.gr",
+								"display_url": "protothema.gr",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 280190,
+				"friends_count": 206,
+				"listed_count": 1650,
+				"created_at": "Fri Mar 20 21:59:48 +0000 2009",
+				"favourites_count": 438,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 450002,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/978350546695356416/27xRdE7K_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/978350546695356416/27xRdE7K_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/25584888/1521996683",
+				"profile_link_color": "F47B23",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "el"
+		},
+		"retweet_count": 3,
+		"favorite_count": 16,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "el",
+		"text": "Κι εμείς είμαστε ευγνώμονες στη Βάσια Πάσχου και όλους της τους συναδέλφους, είτε ιθαγενείς είτε από την Ελλάδα κι αλλού, που εργάζονται ασταμάτητα στο ΕΣΥ μας αυτό τον δύσκολο χρόνο. #πραγματικοί_ήρωες https://t.co/EG28v8ue3q"
+	},
+	{
 		"created_at": "Fri Apr 10 15:56:40 +0000 2020",
 		"id": 1248638926258135000,
 		"id_str": "1248641053508481027",
