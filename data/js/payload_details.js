03eac150b44d9aaa8d17bcd442338684ad819a19
Keith Frankish <k.frankish@gmail.com>
2020-04-25 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2088,
-	"created_at": "Fri Apr 24 00:11:45 +0000 2020",
+	"tweets": 2089,
+	"created_at": "Sat Apr 25 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 24 11:23:30 +0000 2020",
+		"id": 1253610752675647500,
+		"id_str": "1253645737302396930",
+		"full_text": "Would at least three of our Twitter followers/friends please copy and re-post?\nIn these challenging times someone might need one of them. \nChildline: 0800 1111\nSamaritans: 116 123\nDomestic Violence Hotline: 0808 2000 247\nMind: 0300 123 3393\nAge UK: 0800 169 6565\n\nThank you",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1174053397835763700,
+			"id_str": "1174053397835763712",
+			"name": "Keele Philosophy",
+			"screen_name": "KeelePhilosoph1",
+			"location": "Keele, Staffordshire, UK",
+			"description": "News and ideas from the highest philosophy department in the UK @SPGSKeele @KeeleUniversity.",
+			"url": "https://t.co/ZdW6UTXGHW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZdW6UTXGHW",
+							"expanded_url": "https://www.keele.ac.uk/spgs/aboutus/philosophy/",
+							"display_url": "keele.ac.uk/spgs/aboutus/p…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 123,
+			"friends_count": 94,
+			"listed_count": 5,
+			"created_at": "Tue Sep 17 20:13:15 +0000 2019",
+			"favourites_count": 434,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 204,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174994404756590592/_-0iHQFA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174994404756590592/_-0iHQFA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1174053397835763712/1568752437",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Would at least three of our Twitter followers/friends please copy and re-post?\nIn these challenging times someone might need one of them. \nChildline: 0800 1111\nSamaritans: 116 123\nDomestic Violence Hotline: 0808 2000 247\nMind: 0300 123 3393\nAge UK: 0800 169 6565\n\nThank you"
+	},
+	{
 		"created_at": "Thu Apr 23 13:50:25 +0000 2020",
 		"id": 1253313602238533600,
 		"id_str": "1253320323690139648",
