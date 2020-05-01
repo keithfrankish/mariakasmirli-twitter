9e09f93f05f22ca939eafdf524fbb7ca5bfa4423
Keith Frankish <k.frankish@gmail.com>
2020-05-01 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2092,
-	"created_at": "Thu Apr 30 00:11:45 +0000 2020",
+	"tweets": 2093,
+	"created_at": "Fri May 01 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 30 17:06:27 +0000 2020",
+		"id": 1255891285036040200,
+		"id_str": "1255906372065153030",
+		"full_text": "Next Wednesday we'll be hosting our first virtual Q&amp;A from 2-3pm. It's open to anyone with questions about LIS - students, parents, teachers, talking dogs with a penchant for interdisciplinarity, you name it.\n\nRegister below 👇https://t.co/VRxnu7ta4l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VRxnu7ta4l",
+					"expanded_url": "https://www.londoninterdisciplinaryschool.org/events/qa/",
+					"display_url": "londoninterdisciplinaryschool.org/events/qa/",
+					"indices": [
+						230,
+						253
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1088388550243086300,
+			"id_str": "1088388550243086339",
+			"name": "LIS - The London Interdisciplinary School",
+			"screen_name": "weareLIS",
+			"location": "London, England",
+			"description": "There has been an important update to the undergraduate programme. Find out more here: https://t.co/A9nqT6LzGC",
+			"url": "https://t.co/GRInnA0V4W",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GRInnA0V4W",
+							"expanded_url": "https://www.londoninterdisciplinaryschool.org/",
+							"display_url": "londoninterdisciplinaryschool.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/A9nqT6LzGC",
+							"expanded_url": "http://bit.ly/36yjHWF",
+							"display_url": "bit.ly/36yjHWF",
+							"indices": [
+								87,
+								110
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3072,
+			"friends_count": 1254,
+			"listed_count": 23,
+			"created_at": "Thu Jan 24 10:50:26 +0000 2019",
+			"favourites_count": 1031,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1537,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1216698324499210240/XLDrkTvw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1216698324499210240/XLDrkTvw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1088388550243086339/1578918413",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Next Wednesday we'll be hosting our first virtual Q&amp;A from 2-3pm. It's open to anyone with questions about LIS - students, parents, teachers, talking dogs with a penchant for interdisciplinarity, you name it.\n\nRegister below 👇https://t.co/VRxnu7ta4l"
+	},
+	{
 		"created_at": "Wed Apr 29 12:51:45 +0000 2020",
 		"id": 1255479888456417300,
 		"id_str": "1255479888456417302",
