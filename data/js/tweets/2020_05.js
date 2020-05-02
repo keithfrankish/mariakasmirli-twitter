6cbf4e2e0714fd0206d7dff3aa243bcc9349784b
Keith Frankish <k.frankish@gmail.com>
2020-05-02 03:11:45 +0300

    Added 1 tweets

---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,300 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Fri May 01 12:34:04 +0000 2020",
+		"id": 1256191263189106700,
+		"id_str": "1256200211518668802",
+		"full_text": "What? No power naps? https://t.co/Pfdn73wEdk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Pfdn73wEdk",
+					"expanded_url": "https://twitter.com/paulbloomatyale/status/1256180511015346177",
+					"display_url": "twitter.com/paulbloomatyal…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37238862,
+			"id_str": "37238862",
+			"name": "Nigel Warburton",
+			"screen_name": "philosophybites",
+			"location": "Oxford  & London UK",
+			"description": "Founding faculty @weareLIS. A Little History of Philosophy, Philosophy Bites podcast. Consultant editor @aeonmag, @thenewphil, @five_books. @unitedagents",
+			"url": "https://t.co/kD7ridjs8Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kD7ridjs8Q",
+							"expanded_url": "http://www.nigelwarburton.com",
+							"display_url": "nigelwarburton.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 81752,
+			"friends_count": 7183,
+			"listed_count": 2569,
+			"created_at": "Sat May 02 17:20:21 +0000 2009",
+			"favourites_count": 48385,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 49380,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B5B8B4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37238862/1587543534",
+			"profile_link_color": "000DFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1256180511015346200,
+		"quoted_status_id_str": "1256180511015346177",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Pfdn73wEdk",
+			"expanded": "https://twitter.com/paulbloomatyale/status/1256180511015346177",
+			"display": "twitter.com/paulbloomatyal…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 01 11:15:47 +0000 2020",
+			"id": 1256180511015346200,
+			"id_str": "1256180511015346177",
+			"full_text": "Freud's daily routine (from the excellent biography by @PeterKramer) https://t.co/Wu37EvLztd",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				68
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "PeterKramer",
+						"name": "Peter D. Kramer",
+						"id": 27468202,
+						"id_str": "27468202",
+						"indices": [
+							55,
+							67
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1256180411937575000,
+						"id_str": "1256180411937574912",
+						"indices": [
+							69,
+							92
+						],
+						"media_url": "http://pbs.twimg.com/media/EW7ZbdtX0AAr2za.png",
+						"media_url_https": "https://pbs.twimg.com/media/EW7ZbdtX0AAr2za.png",
+						"url": "https://t.co/Wu37EvLztd",
+						"display_url": "pic.twitter.com/Wu37EvLztd",
+						"expanded_url": "https://twitter.com/paulbloomatyale/status/1256180511015346177/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1256180411937575000,
+						"id_str": "1256180411937574912",
+						"indices": [
+							69,
+							92
+						],
+						"media_url": "http://pbs.twimg.com/media/EW7ZbdtX0AAr2za.png",
+						"media_url_https": "https://pbs.twimg.com/media/EW7ZbdtX0AAr2za.png",
+						"url": "https://t.co/Wu37EvLztd",
+						"display_url": "pic.twitter.com/Wu37EvLztd",
+						"expanded_url": "https://twitter.com/paulbloomatyale/status/1256180511015346177/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 663,
+								"h": 302,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 90169053,
+				"id_str": "90169053",
+				"name": "Paul Bloom",
+				"screen_name": "paulbloomatyale",
+				"location": "New Haven, CT",
+				"description": "Psychologist who studies and writes about human nature—including morality, pleasure, and religion",
+				"url": "https://t.co/8baLRXuuqj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/8baLRXuuqj",
+								"expanded_url": "http://campuspress.yale.edu/paulbloom/",
+								"display_url": "campuspress.yale.edu/paulbloom/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 73947,
+				"friends_count": 389,
+				"listed_count": 1539,
+				"created_at": "Sun Nov 15 14:16:00 +0000 2009",
+				"favourites_count": 12724,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1260,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/782699128853704704/U5sCgy1C_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/782699128853704704/U5sCgy1C_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/90169053/1475445994",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 47,
+			"favorite_count": 289,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 47,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What? No power naps? https://t.co/Pfdn73wEdk"
+	}
+]
