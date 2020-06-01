3c90f791da134f5b39a903bfa39e082b4bdf180f
Keith Frankish <k.frankish@gmail.com>
2020-06-01 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2130,
-	"created_at": "Sat May 30 00:11:45 +0000 2020",
+	"tweets": 2131,
+	"created_at": "Mon Jun 01 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 18:00:44 +0000 2020",
+		"id": 1267024206354661400,
+		"id_str": "1267154058441351176",
+		"full_text": "@mariakasmirli Maria great article. I see this happening in my home. Even in a strict grammar school setting, Arran who is doing all his learning remotely with no online interaction yet is suddenly receiving A* almost every week! We as teachers need to rethink strategies when we go back.",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mariakasmirli",
+					"name": "Maria Kasmirli",
+					"id": 103135865,
+					"id_str": "103135865",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1266968843295035400,
+		"in_reply_to_status_id_str": "1266968843295035393",
+		"in_reply_to_user_id": 103135865,
+		"in_reply_to_user_id_str": "103135865",
+		"in_reply_to_screen_name": "mariakasmirli",
+		"user": {
+			"id": 78705838,
+			"id_str": "78705838",
+			"name": "Arita Mukherjee",
+			"screen_name": "aritamukherjee",
+			"location": "London, England",
+			"description": "Take that deep breath and everything else will follow...",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 535,
+			"friends_count": 1482,
+			"listed_count": 14,
+			"created_at": "Wed Sep 30 20:53:52 +0000 2009",
+			"favourites_count": 579,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1284,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1092178411659902976/9gorg0LH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1092178411659902976/9gorg0LH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78705838/1431383240",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@mariakasmirli Maria great article. I see this happening in my home. Even in a strict grammar school setting, Arran who is doing all his learning remotely with no online interaction yet is suddenly receiving A* almost every week! We as teachers need to rethink strategies when we go back."
+	},
+	{
 		"created_at": "Fri May 29 16:26:40 +0000 2020",
 		"id": 1265571301479063600,
 		"id_str": "1266405608087060480",
