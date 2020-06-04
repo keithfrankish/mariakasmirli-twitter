80e4eab920c7fb4d54bd8ef215ec34cfbf211c38
Keith Frankish <k.frankish@gmail.com>
2020-06-04 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2133,
-	"created_at": "Wed Jun 03 00:11:44 +0000 2020",
+	"tweets": 2134,
+	"created_at": "Thu Jun 04 00:11:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 03 19:59:21 +0000 2020",
+		"id": 1267849009340788700,
+		"id_str": "1268271070697738242",
+		"full_text": "Over 100 lesson plans for using children’s literature to inspire philosophical discussions: https://t.co/SnIxbiJMS8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SnIxbiJMS8",
+					"expanded_url": "https://www.philosophyforchildren.org/resources/questions-library/",
+					"display_url": "philosophyforchildren.org/resources/ques…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 239641070,
+			"id_str": "239641070",
+			"name": "Jana Mohr Lone",
+			"screen_name": "JanaMohrLone",
+			"location": "Seattle, Washington",
+			"description": "Philosopher, educator & advocate for children. Director of UW Center for Philosophy for Children, working to empower young people through philosophical inquiry",
+			"url": "http://t.co/icMDIVi8UG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/icMDIVi8UG",
+							"expanded_url": "http://www.philosophyforchildren.org",
+							"display_url": "philosophyforchildren.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4179,
+			"friends_count": 2945,
+			"listed_count": 88,
+			"created_at": "Tue Jan 18 02:51:01 +0000 2011",
+			"favourites_count": 835,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2911,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C7DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3710337083/e77a56b877bb1eb5b0abd00219c1a97c_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3710337083/e77a56b877bb1eb5b0abd00219c1a97c_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/239641070/1393358235",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 36,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Over 100 lesson plans for using children’s literature to inspire philosophical discussions: https://t.co/SnIxbiJMS8"
+	},
+	{
 		"created_at": "Tue Jun 02 19:41:39 +0000 2020",
 		"id": 1267904227743076400,
 		"id_str": "1267904227743076352",
