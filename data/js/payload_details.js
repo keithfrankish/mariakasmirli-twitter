f6572d9119203bd644fe9cc67d4ba362c174862c
Keith Frankish <k.frankish@gmail.com>
2020-06-14 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2143,
-	"created_at": "Fri Jun 12 00:11:45 +0000 2020",
+	"tweets": 2144,
+	"created_at": "Sun Jun 14 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 13 12:45:09 +0000 2020",
+		"id": 1271761865107017700,
+		"id_str": "1271785682160279552",
+		"full_text": "Does philosophy belong to the academy or the community? https://t.co/N3M26UdIW7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/N3M26UdIW7",
+					"expanded_url": "https://www.irishtimes.com/culture/does-philosophy-belong-to-the-academy-or-the-community-1.4271491",
+					"display_url": "irishtimes.com/culture/does-p…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 166988769,
+			"id_str": "166988769",
+			"name": "Joshua Forstenzer",
+			"screen_name": "JoshForstenzer",
+			"location": "Sheffield, England",
+			"description": "Culturally confused French-American philosopher. Dewey scholar. Co-director of the Centre for Engaged Philosophy. All views my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 700,
+			"friends_count": 760,
+			"listed_count": 13,
+			"created_at": "Thu Jul 15 13:50:08 +0000 2010",
+			"favourites_count": 4188,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1252,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/857963777077256192/_zwuGBPM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/857963777077256192/_zwuGBPM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/166988769/1493391314",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Does philosophy belong to the academy or the community? https://t.co/N3M26UdIW7"
+	},
+	{
 		"created_at": "Thu Jun 11 07:12:28 +0000 2020",
 		"id": 1270970323459027000,
 		"id_str": "1270977182769315840",
