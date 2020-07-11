b01fa0fa75a93e3978913ccc728875c0429d0b3c
Keith Frankish <k.frankish@gmail.com>
2020-07-11 03:11:47 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2161,
-	"created_at": "Wed Jul 08 00:11:45 +0000 2020",
+	"tweets": 2162,
+	"created_at": "Sat Jul 11 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 10 14:10:13 +0000 2020",
+		"id": 1281510990509682700,
+		"id_str": "1281591560065384460",
+		"full_text": "Dr. Michael Frazer (@mfrazer78) of UEA dropped by OU Philosophy recently to discuss social explanation and respect for subjects. \n\nWatch Michael's talk here: https://t.co/xlTZpCWJnW https://t.co/BYSCZGu6IW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mfrazer78",
+					"name": "Michael L. Frazer",
+					"id": 2687667817,
+					"id_str": "2687667817",
+					"indices": [
+						20,
+						30
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/xlTZpCWJnW",
+					"expanded_url": "https://archive.org/details/20200603-michael-frazer",
+					"display_url": "archive.org/details/202006…",
+					"indices": [
+						158,
+						181
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1281510816781607000,
+					"id_str": "1281510816781606913",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EcjXTFIWoAEn_aJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcjXTFIWoAEn_aJ.jpg",
+					"url": "https://t.co/BYSCZGu6IW",
+					"display_url": "pic.twitter.com/BYSCZGu6IW",
+					"expanded_url": "https://twitter.com/OU_Philosophy/status/1281510990509682689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1281510816781607000,
+					"id_str": "1281510816781606913",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EcjXTFIWoAEn_aJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcjXTFIWoAEn_aJ.jpg",
+					"url": "https://t.co/BYSCZGu6IW",
+					"display_url": "pic.twitter.com/BYSCZGu6IW",
+					"expanded_url": "https://twitter.com/OU_Philosophy/status/1281510990509682689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 326,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 916231909725933600,
+			"id_str": "916231909725933568",
+			"name": "Open Uni Philosophy",
+			"screen_name": "OU_Philosophy",
+			"location": "",
+			"description": "Philosophy Department at the Faculty of Arts and Social Sciences, Open University",
+			"url": "https://t.co/ktq7G6gtXi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ktq7G6gtXi",
+							"expanded_url": "http://fass.open.ac.uk/philosophy",
+							"display_url": "fass.open.ac.uk/philosophy",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 966,
+			"friends_count": 1149,
+			"listed_count": 24,
+			"created_at": "Fri Oct 06 09:21:24 +0000 2017",
+			"favourites_count": 87,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 201,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/966299652038582272/5gzTcT-g_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/966299652038582272/5gzTcT-g_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/916231909725933568/1507287185",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Michael Frazer (@mfrazer78) of UEA dropped by OU Philosophy recently to discuss social explanation and respect for subjects. \n\nWatch Michael's talk here: https://t.co/xlTZpCWJnW https://t.co/BYSCZGu6IW"
+	},
+	{
 		"created_at": "Tue Jul 07 19:45:06 +0000 2020",
 		"id": 1280226792582905900,
 		"id_str": "1280588673101246465",
