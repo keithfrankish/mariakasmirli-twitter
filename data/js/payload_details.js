6aa4ddfc6fc407e65e59e6f1da0cc310d83c1cfe
Keith Frankish <k.frankish@gmail.com>
2020-08-06 03:11:47 +0300

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2165,
-	"created_at": "Wed Aug 05 00:11:45 +0000 2020",
+	"tweets": 2168,
+	"created_at": "Thu Aug 06 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,334 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Wed Aug 05 15:24:56 +0000 2020",
+		"id": 1291016166627070000,
+		"id_str": "1291032447824658433",
+		"full_text": "Superb animated summaries of key philosophical, political, economic, and psychological theories: https://t.co/Tgh0qRkr8B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Tgh0qRkr8B",
+					"expanded_url": "https://www.bbc.co.uk/programmes/b04bwydw/clips",
+					"display_url": "bbc.co.uk/programmes/b04…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy 4 Schools",
+			"screen_name": "RoyalInstPhilos",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: delivering funded 'intro to philosophy' courses in schools/colleges on behalf of the Royal Institute of Philosophy. P4C Facilitator L3 SAPERE",
+			"url": "https://t.co/P6uOGjR0rz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/P6uOGjR0rz",
+							"expanded_url": "http://royalinstitutephilosophy.org/funding-education/philosophy-in-schools/",
+							"display_url": "royalinstitutephilosophy.org/funding-educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2843,
+			"friends_count": 5002,
+			"listed_count": 43,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 598,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 21,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Superb animated summaries of key philosophical, political, economic, and psychological theories: https://t.co/Tgh0qRkr8B"
+	},
+	{
+		"created_at": "Wed Aug 05 13:04:52 +0000 2020",
+		"id": 1290897505295765500,
+		"id_str": "1290997201078976512",
+		"full_text": "'I've realised my dream': Italy's oldest graduate top of the class at 96&gt;first in philosophy  https://t.co/Kk8PXJkuXu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Kk8PXJkuXu",
+					"expanded_url": "https://www.theguardian.com/world/2020/aug/05/realised-dream-italy-oldest-graduate-top-of-the-class-at-96?CMP=Share_iOSApp_Other",
+					"display_url": "theguardian.com/world/2020/aug…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 351428285,
+			"id_str": "351428285",
+			"name": "Christopher Woodard",
+			"screen_name": "Utiliwood",
+			"location": "",
+			"description": "Philosopher, Nottingham. Ethics and political philosophy. Taking Utilitarianism Seriously OUP 2019 https://t.co/a43vMGvw0C Tweets in private capacity.",
+			"url": "https://t.co/zm5L4W3IOv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zm5L4W3IOv",
+							"expanded_url": "http://christopherwoodard.org",
+							"display_url": "christopherwoodard.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/a43vMGvw0C",
+							"expanded_url": "http://tinyurl.com/y3h42kad",
+							"display_url": "tinyurl.com/y3h42kad",
+							"indices": [
+								99,
+								122
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2099,
+			"friends_count": 2609,
+			"listed_count": 53,
+			"created_at": "Tue Aug 09 07:07:52 +0000 2011",
+			"favourites_count": 5108,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3690,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1142732850489413634/dUc2ccrF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1142732850489413634/dUc2ccrF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/351428285/1567005966",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 49,
+		"favorite_count": 211,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'I've realised my dream': Italy's oldest graduate top of the class at 96&gt;first in philosophy  https://t.co/Kk8PXJkuXu"
+	},
+	{
+		"created_at": "Wed Aug 05 12:59:35 +0000 2020",
+		"id": 1290978903180353500,
+		"id_str": "1290995870868045828",
+		"full_text": "Teachers and philosophy students in schools, the Royal Institute would like to help make your philosophy event happen -- day sessions, conferences, debates, guest speakers, panels, in person or online, we’re open to your ideas.  More info here: https://t.co/ohlfREryiW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ohlfREryiW",
+					"expanded_url": "https://tinyurl.com/y48kwvjj",
+					"display_url": "tinyurl.com/y48kwvjj",
+					"indices": [
+						245,
+						268
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4016524577,
+			"id_str": "4016524577",
+			"name": "Royal Institute of Philosophy",
+			"screen_name": "RIPhilo",
+			"location": "London, England",
+			"description": "The Royal Institute of Philosophy is a charity dedicated to the advancement of philosophy.",
+			"url": "https://t.co/s04q92EMxb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s04q92EMxb",
+							"expanded_url": "http://royalinstitutephilosophy.org",
+							"display_url": "royalinstitutephilosophy.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7601,
+			"friends_count": 477,
+			"listed_count": 121,
+			"created_at": "Thu Oct 22 11:29:25 +0000 2015",
+			"favourites_count": 1513,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1786,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/664854896806600704/QYhP1Mvh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/664854896806600704/QYhP1Mvh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4016524577/1447348714",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 19,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Teachers and philosophy students in schools, the Royal Institute would like to help make your philosophy event happen -- day sessions, conferences, debates, guest speakers, panels, in person or online, we’re open to your ideas.  More info here: https://t.co/ohlfREryiW"
+	},
+	{
 		"created_at": "Tue Aug 04 17:47:35 +0000 2020",
 		"id": 1290705961309679600,
 		"id_str": "1290705961309679616",
