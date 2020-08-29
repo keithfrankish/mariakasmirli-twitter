9d1aff0a0755c7df2d3cb2395e7272709b4b65fe
Keith Frankish <k.frankish@gmail.com>
2020-08-29 03:11:48 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2172,
-	"created_at": "Fri Aug 28 00:11:46 +0000 2020",
+	"tweets": 2173,
+	"created_at": "Sat Aug 29 00:11:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 28 20:25:45 +0000 2020",
+		"id": 1299282885908877300,
+		"id_str": "1299443073160753154",
+		"full_text": "What role can philosophy play in improving the wellbeing of our children? https://t.co/AmNjdubj9g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AmNjdubj9g",
+					"expanded_url": "https://www.theguardian.com/lifeandstyle/2020/aug/28/fear-of-failure-giving-uk-children-lowest-happiness-levels-in-europe",
+					"display_url": "theguardian.com/lifeandstyle/2…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy for Schools",
+			"screen_name": "RoyalInstPhilos",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: delivering funded 'intro to philosophy' courses in schools/colleges on behalf of the Royal Institute of Philosophy. P4C Facilitator L3 SAPERE",
+			"url": "https://t.co/P6uOGjR0rz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/P6uOGjR0rz",
+							"expanded_url": "http://royalinstitutephilosophy.org/funding-education/philosophy-in-schools/",
+							"display_url": "royalinstitutephilosophy.org/funding-educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2879,
+			"friends_count": 4996,
+			"listed_count": 43,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 624,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2069,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What role can philosophy play in improving the wellbeing of our children? https://t.co/AmNjdubj9g"
+	},
+	{
 		"created_at": "Thu Aug 27 20:44:51 +0000 2020",
 		"id": 1298365319673716700,
 		"id_str": "1299085489660719109",
