9459b81e0ea713e97df978004702e4ac6142f799
Keith Frankish <k.frankish@gmail.com>
2020-10-07 03:11:48 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2179,
-	"created_at": "Wed Sep 16 00:11:45 +0000 2020",
+	"tweets": 2180,
+	"created_at": "Wed Oct 07 00:11:46 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Tue Oct 06 16:39:58 +0000 2020",
+		"id": 1313428148776833000,
+		"id_str": "1313519377044697105",
+		"full_text": "One of my aims in life is to help a younger generation of philosophers communicate beyond the narrow confines of academia. I haven’t disclosed this before.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37238862,
+			"id_str": "37238862",
+			"name": "Nigel Warburton",
+			"screen_name": "philosophybites",
+			"location": "Oxford  & London UK",
+			"description": "@weareLIS, A Little History of Philosophy, Philosophy Bites podcast, Consultant Senior Editor @aeonmag, writes @thenewphil, Philosophy Editor @five_books.",
+			"url": "https://t.co/kD7ridjs8Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kD7ridjs8Q",
+							"expanded_url": "http://www.nigelwarburton.com",
+							"display_url": "nigelwarburton.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84232,
+			"friends_count": 7789,
+			"listed_count": 2421,
+			"created_at": "Sat May 02 17:20:21 +0000 2009",
+			"favourites_count": 56946,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B5B8B4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878329460180656128/Ut9ffVks_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37238862/1587543534",
+			"profile_link_color": "000DFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 710,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "One of my aims in life is to help a younger generation of philosophers communicate beyond the narrow confines of academia. I haven’t disclosed this before."
+	}
+]
