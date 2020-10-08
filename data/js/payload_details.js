2175223cfabf018ed59d7b1fa04cd1fe54bc1193
Keith Frankish <k.frankish@gmail.com>
2020-10-08 03:11:48 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2180,
-	"created_at": "Wed Oct 07 00:11:46 +0000 2020",
+	"tweets": 2181,
+	"created_at": "Thu Oct 08 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 07 21:08:42 +0000 2020",
+		"id": 1313829933131722800,
+		"id_str": "1313949396262891520",
+		"full_text": "That’s the translation into Persian of my Causation and explanation. I’m really delighted that the book has appeared in such an ancient language https://t.co/9i1wACFtfz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1313829921656053800,
+					"id_str": "1313829921656053761",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EjupWk-WoAEpxLN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjupWk-WoAEpxLN.jpg",
+					"url": "https://t.co/9i1wACFtfz",
+					"display_url": "pic.twitter.com/9i1wACFtfz",
+					"expanded_url": "https://twitter.com/StathisPsillos/status/1313829933131722753/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1195,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1195,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313829921656053800,
+					"id_str": "1313829921656053761",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EjupWk-WoAEpxLN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjupWk-WoAEpxLN.jpg",
+					"url": "https://t.co/9i1wACFtfz",
+					"display_url": "pic.twitter.com/9i1wACFtfz",
+					"expanded_url": "https://twitter.com/StathisPsillos/status/1313829933131722753/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1195,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1195,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3068008811,
+			"id_str": "3068008811",
+			"name": "Stathis Psillos",
+			"screen_name": "StathisPsillos",
+			"location": "Athens Greece",
+			"description": "Professor of Philosophy of Science & Metaphysics, University of Athens, Greece. I tweet about philosophy, and occasionally politics",
+			"url": "https://t.co/zUI0aPk6f3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zUI0aPk6f3",
+							"expanded_url": "http://www.phs.uoa.gr/psillos",
+							"display_url": "phs.uoa.gr/psillos",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4875,
+			"friends_count": 876,
+			"listed_count": 100,
+			"created_at": "Tue Mar 03 15:27:52 +0000 2015",
+			"favourites_count": 8727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10448,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1268411696458149888/9h6eEz5__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1268411696458149888/9h6eEz5__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3068008811/1549239146",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 90,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That’s the translation into Persian of my Causation and explanation. I’m really delighted that the book has appeared in such an ancient language https://t.co/9i1wACFtfz"
+	},
+	{
 		"created_at": "Tue Oct 06 16:39:58 +0000 2020",
 		"id": 1313428148776833000,
 		"id_str": "1313519377044697105",
