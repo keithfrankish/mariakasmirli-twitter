82f7d8499262ec45a9d0638ff9f0b73d19ff7a0e
Keith Frankish <k.frankish@gmail.com>
2020-11-09 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2184,
-	"created_at": "Sun Nov 01 00:11:45 +0000 2020",
+	"tweets": 2185,
+	"created_at": "Mon Nov 09 00:11:44 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,187 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 08 15:39:46 +0000 2020",
+		"id": 1325129521075458000,
+		"id_str": "1325463026213007366",
+		"full_text": "One down, one to go. https://t.co/TijvQZfwv1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1325129509868220400,
+					"id_str": "1325129509868220422",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/EmPORLUVMAY5fH1.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmPORLUVMAY5fH1.png",
+					"url": "https://t.co/TijvQZfwv1",
+					"display_url": "pic.twitter.com/TijvQZfwv1",
+					"expanded_url": "https://twitter.com/SophieGraceCha1/status/1325129521075458050/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1325129509868220400,
+					"id_str": "1325129509868220422",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/EmPORLUVMAY5fH1.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmPORLUVMAY5fH1.png",
+					"url": "https://t.co/TijvQZfwv1",
+					"display_url": "pic.twitter.com/TijvQZfwv1",
+					"expanded_url": "https://twitter.com/SophieGraceCha1/status/1325129521075458050/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 566,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1243298616007106600,
+			"id_str": "1243298616007106560",
+			"name": "Sophie Grace",
+			"screen_name": "SophieGraceCha1",
+			"location": "Donum Dei",
+			"description": "Cultural-Marxist penguin. Ex-Cub Scout, now Blue Angel. Climber, poet, Extinction Rebel. Author of many words: https://t.co/aqRARLcqYH",
+			"url": "https://t.co/7tOnrmkJK5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7tOnrmkJK5",
+							"expanded_url": "http://www.open.ac.uk/people/tc2973",
+							"display_url": "open.ac.uk/people/tc2973",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/aqRARLcqYH",
+							"expanded_url": "http://open.academia.edu/Sophiegrace",
+							"display_url": "open.academia.edu/Sophiegrace",
+							"indices": [
+								111,
+								134
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1896,
+			"friends_count": 1920,
+			"listed_count": 16,
+			"created_at": "Thu Mar 26 22:07:52 +0000 2020",
+			"favourites_count": 8651,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7949,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245350189327618048/2VgMr5AO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245350189327618048/2VgMr5AO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1243298616007106560/1585665509",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 36,
+		"favorite_count": 172,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "One down, one to go. https://t.co/TijvQZfwv1"
+	}
+]
