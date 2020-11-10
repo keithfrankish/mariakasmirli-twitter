1c54dd337fa8a1ff41a7e1ffcb0ebe6d18c33a55
Keith Frankish <k.frankish@gmail.com>
2020-11-10 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2185,
-	"created_at": "Mon Nov 09 00:11:44 +0000 2020",
+	"tweets": 2186,
+	"created_at": "Tue Nov 10 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 09 07:01:25 +0000 2020",
+		"id": 1320721046358642700,
+		"id_str": "1325694970548932610",
+		"full_text": "This is the most chilling explanation of what Brexit will do to the UK economy after December.\nBy @AdamPosen, President of the Peterson Institute for International Economics. #ThisIsWhatYouVotedFor https://t.co/U4ESjUuVNh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ThisIsWhatYouVotedFor",
+					"indices": [
+						175,
+						197
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AdamPosen",
+					"name": "Adam Posen",
+					"id": 1305556297,
+					"id_str": "1305556297",
+					"indices": [
+						98,
+						108
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1320720011732963300,
+					"id_str": "1320720011732963328",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1320720011732963328/img/-mUSU7TRuiqjFtMm.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1320720011732963328/img/-mUSU7TRuiqjFtMm.jpg",
+					"url": "https://t.co/U4ESjUuVNh",
+					"display_url": "pic.twitter.com/U4ESjUuVNh",
+					"expanded_url": "https://twitter.com/Femi_Sorry/status/1320721046358642689/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1320720011732963300,
+					"id_str": "1320720011732963328",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1320720011732963328/img/-mUSU7TRuiqjFtMm.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1320720011732963328/img/-mUSU7TRuiqjFtMm.jpg",
+					"url": "https://t.co/U4ESjUuVNh",
+					"display_url": "pic.twitter.com/U4ESjUuVNh",
+					"expanded_url": "https://twitter.com/Femi_Sorry/status/1320721046358642689/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 321989,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1320720011732963328/pl/EQwuzXvWBrTk5vZX.m3u8?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1320720011732963328/vid/1280x720/FgHJfwethVreTQ8F.mp4?tag=13"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1320720011732963328/vid/480x270/GeBvg2Gdw_77INjI.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1320720011732963328/vid/640x360/4Dh_54kXaHV2vGXL.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 234694571,
+			"id_str": "234694571",
+			"name": "Femi",
+			"screen_name": "Femi_Sorry",
+			"location": "Solihull, England",
+			"description": "Femi Oluwole\n🇬🇧Law grad \"Do-Gooder\"\nCalling out #Brexit and Boris & Farage's lies & lack of humanity in @Independent/@theIpaper/@LondonEconomic\nEx @OFOCbrexit",
+			"url": "https://t.co/3qVH7HQFJk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3qVH7HQFJk",
+							"expanded_url": "http://Instagram.com/femi_sorry",
+							"display_url": "Instagram.com/femi_sorry",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281533,
+			"friends_count": 1555,
+			"listed_count": 1085,
+			"created_at": "Thu Jan 06 09:50:22 +0000 2011",
+			"favourites_count": 95896,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 70912,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1325322600223019008/ya7G1P-1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1325322600223019008/ya7G1P-1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/234694571/1593978138",
+			"profile_link_color": "B30000",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10317,
+		"favorite_count": 21293,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is the most chilling explanation of what Brexit will do to the UK economy after December.\nBy @AdamPosen, President of the Peterson Institute for International Economics. #ThisIsWhatYouVotedFor https://t.co/U4ESjUuVNh"
+	},
+	{
 		"created_at": "Sun Nov 08 15:39:46 +0000 2020",
 		"id": 1325129521075458000,
 		"id_str": "1325463026213007366",
