6926280931625eeca2c04eca47544a67ccd99a38
Keith Frankish <k.frankish@gmail.com>
2020-12-08 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2190,
-	"created_at": "Thu Dec 03 00:11:45 +0000 2020",
+	"tweets": 2191,
+	"created_at": "Tue Dec 08 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 07 19:02:17 +0000 2020",
+		"id": 1336017309354385400,
+		"id_str": "1336023241614389251",
+		"full_text": "Is a child who tries to be good, but fails, more deserving than a child who is effortlessly good? #santadilemmas https://t.co/fGBjrlTS0f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "santadilemmas",
+					"indices": [
+						98,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1336017303809437700,
+					"id_str": "1336017303809437698",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/Eop8qMdWMAI-LOI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eop8qMdWMAI-LOI.jpg",
+					"url": "https://t.co/fGBjrlTS0f",
+					"display_url": "pic.twitter.com/fGBjrlTS0f",
+					"expanded_url": "https://twitter.com/RoyalInstPhilos/status/1336017309354385408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1336017303809437700,
+					"id_str": "1336017303809437698",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/Eop8qMdWMAI-LOI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eop8qMdWMAI-LOI.jpg",
+					"url": "https://t.co/fGBjrlTS0f",
+					"display_url": "pic.twitter.com/fGBjrlTS0f",
+					"expanded_url": "https://twitter.com/RoyalInstPhilos/status/1336017309354385408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 517,
+							"h": 433,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy for Schools",
+			"screen_name": "RoyalInstPhilos",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: delivering funded 'intro to philosophy' courses in schools/colleges on behalf of the Royal Institute of Philosophy. P4C Facilitator L3 SAPERE",
+			"url": "https://t.co/P6uOGjR0rz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/P6uOGjR0rz",
+							"expanded_url": "http://royalinstitutephilosophy.org/funding-education/philosophy-in-schools/",
+							"display_url": "royalinstitutephilosophy.org/funding-educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3085,
+			"friends_count": 4996,
+			"listed_count": 47,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 699,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2226,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 21,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Is a child who tries to be good, but fails, more deserving than a child who is effortlessly good? #santadilemmas https://t.co/fGBjrlTS0f"
+	},
+	{
 		"created_at": "Wed Dec 02 06:06:43 +0000 2020",
 		"id": 1333512123197091800,
 		"id_str": "1334016123847708672",
