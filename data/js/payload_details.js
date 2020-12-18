9a36d6479eb90352b4b48a42c6b27e6288adbab6
Keith Frankish <k.frankish@gmail.com>
2020-12-18 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2192,
-	"created_at": "Tue Dec 15 00:11:45 +0000 2020",
+	"tweets": 2193,
+	"created_at": "Fri Dec 18 00:11:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,397 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 17 06:42:05 +0000 2020",
+		"id": 1338685475104678000,
+		"id_str": "1339460844439150597",
+		"full_text": "The four thinkers who really reinvented philosophy https://t.co/5CY6CnkiWw https://t.co/YO8ZPbd2ra",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5CY6CnkiWw",
+					"expanded_url": "https://twitter.com/piunsky/status/1338684011628007427",
+					"display_url": "twitter.com/piunsky/status…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1338685371408912400,
+					"id_str": "1338685371408912386",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EpP3QKzXEAIGl7w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpP3QKzXEAIGl7w.jpg",
+					"url": "https://t.co/YO8ZPbd2ra",
+					"display_url": "pic.twitter.com/YO8ZPbd2ra",
+					"expanded_url": "https://twitter.com/smellosopher/status/1338685475104677893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1338685371408912400,
+					"id_str": "1338685371408912386",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EpP3QKzXEAIGl7w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpP3QKzXEAIGl7w.jpg",
+					"url": "https://t.co/YO8ZPbd2ra",
+					"display_url": "pic.twitter.com/YO8ZPbd2ra",
+					"expanded_url": "https://twitter.com/smellosopher/status/1338685475104677893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1338685400458662000,
+					"id_str": "1338685400458661888",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EpP3R3BXEAAchL4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpP3R3BXEAAchL4.jpg",
+					"url": "https://t.co/YO8ZPbd2ra",
+					"display_url": "pic.twitter.com/YO8ZPbd2ra",
+					"expanded_url": "https://twitter.com/smellosopher/status/1338685475104677893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 420,
+							"h": 293,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 420,
+							"h": 293,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 293,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1338685427562270700,
+					"id_str": "1338685427562270724",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EpP3Tb_XUAQ7P2P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpP3Tb_XUAQ7P2P.jpg",
+					"url": "https://t.co/YO8ZPbd2ra",
+					"display_url": "pic.twitter.com/YO8ZPbd2ra",
+					"expanded_url": "https://twitter.com/smellosopher/status/1338685475104677893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 444,
+							"h": 441,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 444,
+							"h": 441,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 444,
+							"h": 441,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1338685451012628500,
+					"id_str": "1338685451012628480",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EpP3UzWXYAAK9FC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpP3UzWXYAAK9FC.jpg",
+					"url": "https://t.co/YO8ZPbd2ra",
+					"display_url": "pic.twitter.com/YO8ZPbd2ra",
+					"expanded_url": "https://twitter.com/smellosopher/status/1338685475104677893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 209,
+							"h": 242,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 209,
+							"h": 242,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 209,
+							"h": 242,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4568484861,
+			"id_str": "4568484861",
+			"name": "Dr. A.S. Barwich",
+			"screen_name": "smellosopher",
+			"location": "The dust bowl of empiricism, IN",
+			"description": "Cognitive scientist & bench philosopher. Book: SMELLOSOPHY (@Harvard_Press July 2020). Asst Prof @IUBloomington. Blog: \"Molecules to Mind\" @PsychToday.",
+			"url": "https://t.co/EwdYcFyp6l",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EwdYcFyp6l",
+							"expanded_url": "http://smellosophy.com",
+							"display_url": "smellosophy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5841,
+			"friends_count": 3579,
+			"listed_count": 125,
+			"created_at": "Tue Dec 22 14:27:00 +0000 2015",
+			"favourites_count": 42137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27005,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1299369568368865287/c4y70gWp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1299369568368865287/c4y70gWp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4568484861/1597321732",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1338684011628007400,
+		"quoted_status_id_str": "1338684011628007427",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5CY6CnkiWw",
+			"expanded": "https://twitter.com/piunsky/status/1338684011628007427",
+			"display": "twitter.com/piunsky/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Dec 15 03:15:14 +0000 2020",
+			"id": 1338684011628007400,
+			"id_str": "1338684011628007427",
+			"full_text": "The four thinkers who reinvented philosophy https://t.co/dKXt8biosH",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				67
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/dKXt8biosH",
+						"expanded_url": "https://www.newstatesman.com/time-magicians-invention-modern-thought-wolfram-eilenberger-review",
+						"display_url": "newstatesman.com/time-magicians…",
+						"indices": [
+							44,
+							67
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 130325162,
+				"id_str": "130325162",
+				"name": "Luis Xavier",
+				"screen_name": "piunsky",
+				"location": "",
+				"description": "Philosopher, Editor, Reader. Arabic Philosophy & Thought. Intellectual History. Aesthetics. The City. Migration and Migraine. @topicosrevista",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1224,
+				"friends_count": 758,
+				"listed_count": 41,
+				"created_at": "Wed Apr 07 00:15:11 +0000 2010",
+				"favourites_count": 2926,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 19912,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/913907994865184768/vJuQSYLV_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/913907994865184768/vJuQSYLV_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/130325162/1401498054",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 22,
+		"favorite_count": 112,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The four thinkers who really reinvented philosophy https://t.co/5CY6CnkiWw https://t.co/YO8ZPbd2ra"
+	},
+	{
 		"created_at": "Mon Dec 14 05:24:58 +0000 2020",
 		"id": 1337867953744777200,
 		"id_str": "1338354273718050816",
