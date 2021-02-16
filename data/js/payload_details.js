3e38e3d604bf17e1e02313401f9ab46d089c6e11
Keith Frankish <k.frankish@gmail.com>
2021-02-16 02:11:45 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2206,
-	"created_at": "Fri Jan 29 00:11:44 +0000 2021",
+	"tweets": 2207,
+	"created_at": "Tue Feb 16 00:11:44 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,137 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Mon Feb 15 19:48:26 +0000 2021",
+		"id": 1361252070028767200,
+		"id_str": "1361402005026004999",
+		"full_text": "Here's a #HotTip for early career researchers:\n\nIf you think it will take X months to get a paper published in a top journal, a more realistic estimate is X*5. Seriously.\n\nThe sooner you realize this, the greater your chances of success.\n\n#AcademicTwitter #PhDChat #PhDLife",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HotTip",
+					"indices": [
+						9,
+						16
+					]
+				},
+				{
+					"text": "AcademicTwitter",
+					"indices": [
+						239,
+						255
+					]
+				},
+				{
+					"text": "PhDChat",
+					"indices": [
+						256,
+						264
+					]
+				},
+				{
+					"text": "PhDLife",
+					"indices": [
+						265,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 908226487928594400,
+			"id_str": "908226487928594432",
+			"name": "Prof. Andrew R. Timming",
+			"screen_name": "timminglab",
+			"location": "Melbourne, Australia",
+			"description": "Professor of Human Resource Management. Editor-in-chief at https://t.co/1PdOHGM4ja. Views are mine and not those of my employer.",
+			"url": "https://t.co/jpnsctPUtf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jpnsctPUtf",
+							"expanded_url": "http://www.timminglab.com",
+							"display_url": "timminglab.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/1PdOHGM4ja",
+							"expanded_url": "http://www.dire-ed.com",
+							"display_url": "dire-ed.com",
+							"indices": [
+								59,
+								82
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 20146,
+			"friends_count": 16139,
+			"listed_count": 89,
+			"created_at": "Thu Sep 14 07:10:43 +0000 2017",
+			"favourites_count": 1187,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1773,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/908236768809381888/ZhxvF8Cc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/908226487928594432/1560415816",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 293,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Here's a #HotTip for early career researchers:\n\nIf you think it will take X months to get a paper published in a top journal, a more realistic estimate is X*5. Seriously.\n\nThe sooner you realize this, the greater your chances of success.\n\n#AcademicTwitter #PhDChat #PhDLife"
+	}
+]
