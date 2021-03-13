9625d9d9a5daedfcc94d92b6ea75f5dca256d4a6
Keith Frankish <k.frankish@gmail.com>
2021-03-13 02:11:46 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2210,
-	"created_at": "Mon Mar 08 00:11:44 +0000 2021",
+	"tweets": 2211,
+	"created_at": "Sat Mar 13 00:11:44 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Fri Mar 12 17:36:51 +0000 2021",
+		"id": 1370425596493332500,
+		"id_str": "1370428587573121029",
+		"full_text": "Let's go, let's go, let's go https://t.co/guHud79LoE https://t.co/UVILMy0ENM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/guHud79LoE",
+					"expanded_url": "https://sites.uci.edu/identitiesandepistemicinjustice/schedule/",
+					"display_url": "sites.uci.edu/identitiesande…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1370425590281609200,
+					"id_str": "1370425590281609217",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/EwS60EPXIAEdknR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwS60EPXIAEdknR.jpg",
+					"url": "https://t.co/UVILMy0ENM",
+					"display_url": "pic.twitter.com/UVILMy0ENM",
+					"expanded_url": "https://twitter.com/csandis/status/1370425596493332483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1370425590281609200,
+					"id_str": "1370425590281609217",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/EwS60EPXIAEdknR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwS60EPXIAEdknR.jpg",
+					"url": "https://t.co/UVILMy0ENM",
+					"display_url": "pic.twitter.com/UVILMy0ENM",
+					"expanded_url": "https://twitter.com/csandis/status/1370425596493332483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 175423518,
+			"id_str": "175423518",
+			"name": "Constantine Sandis",
+			"screen_name": "csandis",
+			"location": "London, England",
+			"description": "Prof @UHPhilosophy Fellow @theRSAorg Founding Editor @lex_academic Opinions are overrated PhilAction, XAI, Wittgenstein, Understanding Others 🌱",
+			"url": "https://t.co/WtL1WsdJwT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WtL1WsdJwT",
+							"expanded_url": "https://constantinesandis.academia.edu",
+							"display_url": "constantinesandis.academia.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9267,
+			"friends_count": 4670,
+			"listed_count": 225,
+			"created_at": "Fri Aug 06 15:27:48 +0000 2010",
+			"favourites_count": 27212,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13201,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/632848235028160512/KHtwzHkS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/632848235028160512/KHtwzHkS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/175423518/1615048227",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 12,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Let's go, let's go, let's go https://t.co/guHud79LoE https://t.co/UVILMy0ENM"
+	},
+	{
 		"created_at": "Sun Mar 07 06:27:51 +0000 2021",
 		"id": 1368367860129673200,
 		"id_str": "1368448289209729031",
