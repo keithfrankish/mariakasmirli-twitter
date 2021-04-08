8623524f072b6a5908755ce50451658d19fee3a5
Keith Frankish <k.frankish@gmail.com>
2021-04-08 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2218,
-	"created_at": "Mon Mar 29 00:11:44 +0000 2021",
+	"tweets": 2219,
+	"created_at": "Thu Apr 08 00:11:44 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,187 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Wed Apr 07 03:47:15 +0000 2021",
+		"id": 1379555201762332700,
+		"id_str": "1379641897937997826",
+		"full_text": "Jean Piaget’s study makes me feel a whole lot better about myself. https://t.co/xk39cAFJUv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1379555196947292200,
+					"id_str": "1379555196947292160",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EyUqJHYWYAAS8P6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EyUqJHYWYAAS8P6.jpg",
+					"url": "https://t.co/xk39cAFJUv",
+					"display_url": "pic.twitter.com/xk39cAFJUv",
+					"expanded_url": "https://twitter.com/Philos4Schools/status/1379555201762332683/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1181,
+							"h": 1869,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 758,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1379555196947292200,
+					"id_str": "1379555196947292160",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EyUqJHYWYAAS8P6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EyUqJHYWYAAS8P6.jpg",
+					"url": "https://t.co/xk39cAFJUv",
+					"display_url": "pic.twitter.com/xk39cAFJUv",
+					"expanded_url": "https://twitter.com/Philos4Schools/status/1379555201762332683/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1181,
+							"h": 1869,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 758,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2624007323,
+			"id_str": "2624007323",
+			"name": "Philosophy for Schools",
+			"screen_name": "Philos4Schools",
+			"location": "North West, England",
+			"description": "Glenn Skelhorn: philosophy courses/events online and in schools; Royal Institute of Philosophy tutor; P4C Facilitator L3 SAPERE https://t.co/qDG0ljpVE6",
+			"url": "https://t.co/qDG0ljpVE6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qDG0ljpVE6",
+							"expanded_url": "http://www.philosophyforteens.com",
+							"display_url": "philosophyforteens.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/qDG0ljpVE6",
+							"expanded_url": "http://www.philosophyforteens.com",
+							"display_url": "philosophyforteens.com",
+							"indices": [
+								128,
+								151
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3286,
+			"friends_count": 5003,
+			"listed_count": 51,
+			"created_at": "Thu Jun 19 22:13:01 +0000 2014",
+			"favourites_count": 821,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168220747535454213/89N0ZKQn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2624007323/1508108769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 33,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Jean Piaget’s study makes me feel a whole lot better about myself. https://t.co/xk39cAFJUv"
+	}
+]
