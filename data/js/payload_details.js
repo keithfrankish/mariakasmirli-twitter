8e8be590783f8d4b85688b04f5bde0dc5de1a56d
Keith Frankish <k.frankish@gmail.com>
2021-04-20 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2221,
-	"created_at": "Thu Apr 15 00:11:44 +0000 2021",
+	"tweets": 2222,
+	"created_at": "Tue Apr 20 00:11:44 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,400 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 19 05:25:16 +0000 2021",
+		"id": 1383813089104076800,
+		"id_str": "1384015220331270149",
+		"full_text": "Every life! https://t.co/A1TeVFd743",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/A1TeVFd743",
+					"expanded_url": "https://twitter.com/OdedRechavi/status/1383636588794499076",
+					"display_url": "twitter.com/OdedRechavi/st…",
+					"indices": [
+						12,
+						35
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52522040,
+			"id_str": "52522040",
+			"name": "Keith Frankish",
+			"screen_name": "keithfrankish",
+			"location": "Heraklion, Crete, Greece",
+			"description": "Philosopher & writer. European. Honorary Reader @sheffielduni. Interests in consciousness, reasoning, belief. Tweeting phil mind, cog sci, Greece, links & chat.",
+			"url": "http://t.co/tXhT44huzC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tXhT44huzC",
+							"expanded_url": "http://www.keithfrankish.com",
+							"display_url": "keithfrankish.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15460,
+			"friends_count": 2244,
+			"listed_count": 511,
+			"created_at": "Tue Jun 30 21:10:19 +0000 2009",
+			"favourites_count": 44250,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 64410,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "304075",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207775707839041538/osgc7L9X_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52522040/1554843010",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "C0C6CC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1383636588794499000,
+		"quoted_status_id_str": "1383636588794499076",
+		"quoted_status_permalink": {
+			"url": "https://t.co/A1TeVFd743",
+			"expanded": "https://twitter.com/OdedRechavi/status/1383636588794499076",
+			"display": "twitter.com/OdedRechavi/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Apr 18 04:20:44 +0000 2021",
+			"id": 1383636588794499000,
+			"id_str": "1383636588794499076",
+			"full_text": "Every PhD  https://t.co/3fV1UN6lK8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				34
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1383518636925591600,
+						"id_str": "1383518636925591557",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1383518636925591557/pu/img/yWwOThCXvdaoYZjQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1383518636925591557/pu/img/yWwOThCXvdaoYZjQ.jpg",
+						"url": "https://t.co/3fV1UN6lK8",
+						"display_url": "pic.twitter.com/3fV1UN6lK8",
+						"expanded_url": "https://twitter.com/urupzia/status/1383518691963260933/video/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1383518691963261000,
+						"source_status_id_str": "1383518691963260933",
+						"source_user_id": 259287317,
+						"source_user_id_str": "259287317"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1383518636925591600,
+						"id_str": "1383518636925591557",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1383518636925591557/pu/img/yWwOThCXvdaoYZjQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1383518636925591557/pu/img/yWwOThCXvdaoYZjQ.jpg",
+						"url": "https://t.co/3fV1UN6lK8",
+						"display_url": "pic.twitter.com/3fV1UN6lK8",
+						"expanded_url": "https://twitter.com/urupzia/status/1383518691963260933/video/1",
+						"type": "video",
+						"sizes": {
+							"small": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 460,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1383518691963261000,
+						"source_status_id_str": "1383518691963260933",
+						"source_user_id": 259287317,
+						"source_user_id_str": "259287317",
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 19600,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1383518636925591557/pu/vid/460x460/H_y8tM5GRQcgPR9k.mp4?tag=12"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1383518636925591557/pu/pl/CZrhRYppjOSFPh4J.m3u8?tag=12"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1383518636925591557/pu/vid/320x320/vYMui9cXvNStAo5u.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false,
+							"source_user": {
+								"id": 259287317,
+								"id_str": "259287317",
+								"name": "Victoriano Aizpurú",
+								"screen_name": "urupzia",
+								"location": "Tandil, Argentina",
+								"description": "Sí, mi nombre es Victoriano.",
+								"url": "https://t.co/bczralngSr",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "https://t.co/bczralngSr",
+												"expanded_url": "http://instagr.am/urupzia",
+												"display_url": "instagr.am/urupzia",
+												"indices": [
+													0,
+													23
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": []
+									}
+								},
+								"protected": false,
+								"followers_count": 27003,
+								"friends_count": 2874,
+								"listed_count": 89,
+								"created_at": "Tue Mar 01 15:08:58 +0000 2011",
+								"favourites_count": 4,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": false,
+								"verified": false,
+								"statuses_count": 7881,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": true,
+								"is_translation_enabled": true,
+								"profile_background_color": "131516",
+								"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+								"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+								"profile_background_tile": true,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/1368352135147094020/vi6o0kUW_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368352135147094020/vi6o0kUW_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/259287317/1451704356",
+								"profile_link_color": "131516",
+								"profile_sidebar_border_color": "000000",
+								"profile_sidebar_fill_color": "000000",
+								"profile_text_color": "000000",
+								"profile_use_background_image": true,
+								"has_extended_profile": true,
+								"default_profile": false,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "badged",
+								"withheld_in_countries": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 702785870710095900,
+				"id_str": "702785870710095872",
+				"name": "Oded Rechavi 🦉",
+				"screen_name": "OdedRechavi",
+				"location": "Israel",
+				"description": "Prof @TelAvivUni studying memory inheritance, Dead Sea Scrolls’ ancient DNA, irrational worms & engineered brain parasites @TEDx https://t.co/3BdGAecwOp",
+				"url": "https://t.co/J5pZeJuxZL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/J5pZeJuxZL",
+								"expanded_url": "http://odedrechavilab.com",
+								"display_url": "odedrechavilab.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/3BdGAecwOp",
+								"expanded_url": "https://youtu.be/SMbrASDmXXk",
+								"display_url": "youtu.be/SMbrASDmXXk",
+								"indices": [
+									129,
+									152
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 40598,
+				"friends_count": 1099,
+				"listed_count": 163,
+				"created_at": "Thu Feb 25 09:23:03 +0000 2016",
+				"favourites_count": 80901,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 29260,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1371110855304167436/CqLA4KUp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371110855304167436/CqLA4KUp_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/702785870710095872/1525277320",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1989,
+			"favorite_count": 7540,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 33,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Every life! https://t.co/A1TeVFd743"
+	},
+	{
 		"created_at": "Wed Apr 14 15:52:36 +0000 2021",
 		"id": 1382031227713097700,
 		"id_str": "1382361152424976384",
