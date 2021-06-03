8ef627cc6e7eba0bd9a71ae830048a55ea67f1fa
Keith Frankish <k.frankish@gmail.com>
2021-06-03 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2236,
-	"created_at": "Tue Jun 01 00:11:45 +0000 2021",
+	"tweets": 2237,
+	"created_at": "Thu Jun 03 00:11:44 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Wed Jun 02 16:01:02 +0000 2021",
+		"id": 1400091023347433500,
+		"id_str": "1400120279024680962",
+		"full_text": "When I was a kid, there were no cellphones or tablets. We read the cereal box at breakfast. https://t.co/EWdqUdtEF1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1400091020180836400,
+					"id_str": "1400091020180836353",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E24fXWcX0AEQ7Tn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E24fXWcX0AEQ7Tn.jpg",
+					"url": "https://t.co/EWdqUdtEF1",
+					"display_url": "pic.twitter.com/EWdqUdtEF1",
+					"expanded_url": "https://twitter.com/teachergoals/status/1400091023347433479/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 820,
+							"h": 461,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 820,
+							"h": 461,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1400091020180836400,
+					"id_str": "1400091020180836353",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E24fXWcX0AEQ7Tn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E24fXWcX0AEQ7Tn.jpg",
+					"url": "https://t.co/EWdqUdtEF1",
+					"display_url": "pic.twitter.com/EWdqUdtEF1",
+					"expanded_url": "https://twitter.com/teachergoals/status/1400091023347433479/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 820,
+							"h": 461,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 820,
+							"h": 461,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2485527848,
+			"id_str": "2485527848",
+			"name": "TeacherGoals",
+			"screen_name": "teachergoals",
+			"location": "",
+			"description": "Posting what's funny, frustrating, and fantastic about education. #teachergoals",
+			"url": "https://t.co/sKX6Zwch7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sKX6Zwch7U",
+							"expanded_url": "http://www.teachergoals.org/",
+							"display_url": "teachergoals.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 232341,
+			"friends_count": 46277,
+			"listed_count": 811,
+			"created_at": "Fri May 09 15:24:31 +0000 2014",
+			"favourites_count": 40545,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13054,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366910294593859586/XP2zIDXa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366910294593859586/XP2zIDXa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2485527848/1543365207",
+			"profile_link_color": "89C9FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 318,
+		"favorite_count": 2521,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When I was a kid, there were no cellphones or tablets. We read the cereal box at breakfast. https://t.co/EWdqUdtEF1"
+	}
+]
