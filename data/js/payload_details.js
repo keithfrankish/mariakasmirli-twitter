4bc41a0045010b11666e0440f2004e3a7df492e3
Keith Frankish <k.frankish@gmail.com>
2021-06-06 03:11:45 +0300

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2237,
-	"created_at": "Thu Jun 03 00:11:44 +0000 2021",
+	"tweets": 2240,
+	"created_at": "Sun Jun 06 00:11:44 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,565 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 05 13:36:17 +0000 2021",
+		"id": 1401168638749593600,
+		"id_str": "1401171016433475590",
+		"full_text": "Our hard #3A work is paying off! https://t.co/i6KvCo6j2l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "3A",
+					"indices": [
+						9,
+						12
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/i6KvCo6j2l",
+					"expanded_url": "https://twitter.com/theonion/status/1401162148043276291",
+					"display_url": "twitter.com/theonion/statu…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 36651871,
+			"id_str": "36651871",
+			"name": "Elizabeth Picciuto",
+			"screen_name": "epicciuto",
+			"location": "Extremely Long Island",
+			"description": "Writer, wife, mom of three boys—one with multiple disabilities. Philosophy PhD. Rooted cosmopolitan. Will garden for food and flowers. #3A!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15520,
+			"friends_count": 3081,
+			"listed_count": 251,
+			"created_at": "Thu Apr 30 12:40:01 +0000 2009",
+			"favourites_count": 154684,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 117241,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374799430877085699/FB5EiGUE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374799430877085699/FB5EiGUE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/36651871/1513283439",
+			"profile_link_color": "94D487",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1401162148043276300,
+		"quoted_status_id_str": "1401162148043276291",
+		"quoted_status_permalink": {
+			"url": "https://t.co/i6KvCo6j2l",
+			"expanded": "https://twitter.com/theonion/status/1401162148043276291",
+			"display": "twitter.com/theonion/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jun 05 13:01:03 +0000 2021",
+			"id": 1401162148043276300,
+			"id_str": "1401162148043276291",
+			"full_text": "Third Amendment Rights Group Celebrates Another Successful Year https://t.co/5XVSxQKZom https://t.co/kjOZQK2JlH",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				87
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/5XVSxQKZom",
+						"expanded_url": "https://bit.ly/3vYdmRm",
+						"display_url": "bit.ly/3vYdmRm",
+						"indices": [
+							64,
+							87
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1401162146231242800,
+						"id_str": "1401162146231242752",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/E3HtjF7WUAAFSN4.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E3HtjF7WUAAFSN4.jpg",
+						"url": "https://t.co/kjOZQK2JlH",
+						"display_url": "pic.twitter.com/kjOZQK2JlH",
+						"expanded_url": "https://twitter.com/TheOnion/status/1401162148043276291/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1401162146231242800,
+						"id_str": "1401162146231242752",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/E3HtjF7WUAAFSN4.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E3HtjF7WUAAFSN4.jpg",
+						"url": "https://t.co/kjOZQK2JlH",
+						"display_url": "pic.twitter.com/kjOZQK2JlH",
+						"expanded_url": "https://twitter.com/TheOnion/status/1401162148043276291/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 470,
+								"h": 359,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14075928,
+				"id_str": "14075928",
+				"name": "The Onion",
+				"screen_name": "TheOnion",
+				"location": "",
+				"description": "America's Finest News Source.",
+				"url": "https://t.co/EIF811CxOS",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EIF811CxOS",
+								"expanded_url": "https://www.theonion.com/",
+								"display_url": "theonion.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11659996,
+				"friends_count": 8,
+				"listed_count": 68699,
+				"created_at": "Tue Mar 04 02:48:37 +0000 2008",
+				"favourites_count": 3,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 81475,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/875392068125769732/yrN-1k0Y_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/875392068125769732/yrN-1k0Y_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14075928/1617209332",
+				"profile_link_color": "327144",
+				"profile_sidebar_border_color": "F2E195",
+				"profile_sidebar_fill_color": "FFF7CC",
+				"profile_text_color": "0C3E53",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1472,
+			"favorite_count": 13973,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 8,
+		"favorite_count": 55,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our hard #3A work is paying off! https://t.co/i6KvCo6j2l"
+	},
+	{
+		"created_at": "Sat Jun 05 06:15:55 +0000 2021",
+		"id": 1401056189434241000,
+		"id_str": "1401060196722618373",
+		"full_text": "The humanist community (including our patrons @stephenfry @theAliceRoberts @jimalkhalili @sanditoksvig &amp; many more) has come together to urge as many people as possible to get vaccinated against Covid-19. Bring an end to this pandemic, vaccinate.\n\nhttps://t.co/WpMlyd022f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stephenfry",
+					"name": "Stephen Fry",
+					"id": 15439395,
+					"id_str": "15439395",
+					"indices": [
+						46,
+						57
+					]
+				},
+				{
+					"screen_name": "theAliceRoberts",
+					"name": "Prof Alice Roberts💙",
+					"id": 260211154,
+					"id_str": "260211154",
+					"indices": [
+						58,
+						74
+					]
+				},
+				{
+					"screen_name": "jimalkhalili",
+					"name": "Jim Al-Khalili",
+					"id": 104786280,
+					"id_str": "104786280",
+					"indices": [
+						75,
+						88
+					]
+				},
+				{
+					"screen_name": "sanditoksvig",
+					"name": "Sandi Toksvig",
+					"id": 28680544,
+					"id_str": "28680544",
+					"indices": [
+						89,
+						102
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/WpMlyd022f",
+					"expanded_url": "https://humanism.org.uk/2021/06/02/humanists-and-christians-urge-as-many-as-possible-to-get-vaccinated-against-covid-19/",
+					"display_url": "humanism.org.uk/2021/06/02/hum…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 46686058,
+			"id_str": "46686058",
+			"name": "Humanists UK",
+			"screen_name": "Humanists_UK",
+			"location": "London",
+			"description": "We advance free thinking and promote humanism to create a tolerant society where rational thinking and kindness prevail. Registered charity no. 285987.",
+			"url": "https://t.co/o6YMksqCGI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o6YMksqCGI",
+							"expanded_url": "http://humanists.uk",
+							"display_url": "humanists.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 104949,
+			"friends_count": 3078,
+			"listed_count": 1181,
+			"created_at": "Fri Jun 12 16:18:39 +0000 2009",
+			"favourites_count": 13496,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 52980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1155811722529333249/Je9uTjfV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155811722529333249/Je9uTjfV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/46686058/1621337265",
+			"profile_link_color": "4C4184",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "AAD3D5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 82,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The humanist community (including our patrons @stephenfry @theAliceRoberts @jimalkhalili @sanditoksvig &amp; many more) has come together to urge as many people as possible to get vaccinated against Covid-19. Bring an end to this pandemic, vaccinate.\n\nhttps://t.co/WpMlyd022f"
+	},
+	{
+		"created_at": "Sat Jun 05 05:59:58 +0000 2021",
+		"id": 1401055936106664000,
+		"id_str": "1401056181938966530",
+		"full_text": "3 feelings that can cause serious damage if ignored instead of acknowledged—and how to deal with them, writes @GroverSean https://t.co/Ap1P8hZWr7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GroverSean",
+					"name": "Sean Grover",
+					"id": 450395551,
+					"id_str": "450395551",
+					"indices": [
+						110,
+						121
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ap1P8hZWr7",
+					"expanded_url": "https://bit.ly/366rm0B",
+					"display_url": "bit.ly/366rm0B",
+					"indices": [
+						122,
+						145
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27726303,
+			"id_str": "27726303",
+			"name": "Psychology Today",
+			"screen_name": "PsychToday",
+			"location": "New York, NY",
+			"description": "Insight about everybody's favorite subject: Ourselves.",
+			"url": "http://t.co/IdmfB9aX4p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/IdmfB9aX4p",
+							"expanded_url": "http://www.psychologytoday.com",
+							"display_url": "psychologytoday.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 775378,
+			"friends_count": 974,
+			"listed_count": 9603,
+			"created_at": "Mon Mar 30 20:53:58 +0000 2009",
+			"favourites_count": 152,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 48550,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389564158866243585/TgpXFsdz_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389564158866243585/TgpXFsdz_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27726303/1620132887",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "080707",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 59,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "3 feelings that can cause serious damage if ignored instead of acknowledged—and how to deal with them, writes @GroverSean https://t.co/Ap1P8hZWr7"
+	},
+	{
 		"created_at": "Wed Jun 02 16:01:02 +0000 2021",
 		"id": 1400091023347433500,
 		"id_str": "1400120279024680962",
