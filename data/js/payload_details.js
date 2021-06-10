9bf70ee0c4c84f6423158441c262842b67425932
Keith Frankish <k.frankish@gmail.com>
2021-06-10 03:11:45 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2241,
-	"created_at": "Mon Jun 07 00:11:44 +0000 2021",
+	"tweets": 2242,
+	"created_at": "Thu Jun 10 00:11:44 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 09 17:21:46 +0000 2021",
+		"id": 1402324218038173700,
+		"id_str": "1402677313821609988",
+		"full_text": "The pandemic affected every kid. \n\nStop worrying about them being behind. \n\nThey aren't behind anyone.\n\nThank you for coming to my Ted Talk.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2485527848,
+			"id_str": "2485527848",
+			"name": "TeacherGoals",
+			"screen_name": "teachergoals",
+			"location": "",
+			"description": "Posting what's funny, frustrating, and fantastic about education. #teachergoals",
+			"url": "https://t.co/sKX6Zwch7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sKX6Zwch7U",
+							"expanded_url": "http://www.teachergoals.org/",
+							"display_url": "teachergoals.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 235553,
+			"friends_count": 46293,
+			"listed_count": 821,
+			"created_at": "Fri May 09 15:24:31 +0000 2014",
+			"favourites_count": 40559,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366910294593859586/XP2zIDXa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366910294593859586/XP2zIDXa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2485527848/1543365207",
+			"profile_link_color": "89C9FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 217,
+		"favorite_count": 1331,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The pandemic affected every kid. \n\nStop worrying about them being behind. \n\nThey aren't behind anyone.\n\nThank you for coming to my Ted Talk."
+	},
+	{
 		"created_at": "Sun Jun 06 13:35:15 +0000 2021",
 		"id": 1401326668409618400,
 		"id_str": "1401533144382193665",
