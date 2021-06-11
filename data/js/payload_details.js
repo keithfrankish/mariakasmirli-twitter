4966f831976c2a91e337d0b91ecf37674c5d6732
Keith Frankish <k.frankish@gmail.com>
2021-06-11 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2242,
-	"created_at": "Thu Jun 10 00:11:44 +0000 2021",
+	"tweets": 2243,
+	"created_at": "Fri Jun 11 00:11:45 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 10 13:59:30 +0000 2021",
+		"id": 1402702270689599500,
+		"id_str": "1402988799186006030",
+		"full_text": "We should strive to be lifelong learners💡 https://t.co/omu3zbirLz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1402702268936425500,
+					"id_str": "1402702268936425475",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E3dmSCGXEAMSube.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3dmSCGXEAMSube.jpg",
+					"url": "https://t.co/omu3zbirLz",
+					"display_url": "pic.twitter.com/omu3zbirLz",
+					"expanded_url": "https://twitter.com/TED_ED/status/1402702270689599495/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 678,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1196,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1196,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1402702268936425500,
+					"id_str": "1402702268936425475",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E3dmSCGXEAMSube.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3dmSCGXEAMSube.jpg",
+					"url": "https://t.co/omu3zbirLz",
+					"display_url": "pic.twitter.com/omu3zbirLz",
+					"expanded_url": "https://twitter.com/TED_ED/status/1402702270689599495/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 678,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1196,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1196,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 259403751,
+			"id_str": "259403751",
+			"name": "TED-Ed",
+			"screen_name": "TED_ED",
+			"location": "",
+			"description": "Our mission is to spark and celebrate the ideas of students and teachers throughout the world. Follow for program updates + new animated videos every week!",
+			"url": "https://t.co/OFKq7hBUjN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OFKq7hBUjN",
+							"expanded_url": "http://ed.ted.com",
+							"display_url": "ed.ted.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 529337,
+			"friends_count": 247,
+			"listed_count": 5755,
+			"created_at": "Tue Mar 01 21:09:07 +0000 2011",
+			"favourites_count": 3834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11043,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EEEEEE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/897879020645298178/SHglmmjp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/897879020645298178/SHglmmjp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/259403751/1502812845",
+			"profile_link_color": "FF2B06",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 324,
+		"favorite_count": 1199,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We should strive to be lifelong learners💡 https://t.co/omu3zbirLz"
+	},
+	{
 		"created_at": "Wed Jun 09 17:21:46 +0000 2021",
 		"id": 1402324218038173700,
 		"id_str": "1402677313821609988",
