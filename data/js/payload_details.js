ac152cb55e75c721c25c9090ef46e5448e29a135
Keith Frankish <k.frankish@gmail.com>
2021-06-22 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2250,
-	"created_at": "Fri Jun 18 00:11:44 +0000 2021",
+	"tweets": 2251,
+	"created_at": "Tue Jun 22 00:11:44 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 21 03:43:30 +0000 2021",
+		"id": 1406586914677956600,
+		"id_str": "1406820043980218368",
+		"full_text": "Teachers ~ https://t.co/reSzmMfT0T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1406586910739476500,
+					"id_str": "1406586910739476481",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/E4UzV8hXMAEcuzI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4UzV8hXMAEcuzI.jpg",
+					"url": "https://t.co/reSzmMfT0T",
+					"display_url": "pic.twitter.com/reSzmMfT0T",
+					"expanded_url": "https://twitter.com/SteeleThoughts/status/1406586914677956611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1406586910739476500,
+					"id_str": "1406586910739476481",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/E4UzV8hXMAEcuzI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4UzV8hXMAEcuzI.jpg",
+					"url": "https://t.co/reSzmMfT0T",
+					"display_url": "pic.twitter.com/reSzmMfT0T",
+					"expanded_url": "https://twitter.com/SteeleThoughts/status/1406586914677956611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 717067922158411800,
+			"id_str": "717067922158411777",
+			"name": "Danny Steele",
+			"screen_name": "SteeleThoughts",
+			"location": "Birmingham, AL",
+			"description": "Educator / Speaker / Author / I love students, teachers, school culture, leadership, and public education. That’s what I tweet about.",
+			"url": "https://t.co/YlWDcw18aj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YlWDcw18aj",
+							"expanded_url": "http://www.steelethoughts.com",
+							"display_url": "steelethoughts.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 104017,
+			"friends_count": 3967,
+			"listed_count": 569,
+			"created_at": "Mon Apr 04 19:14:49 +0000 2016",
+			"favourites_count": 84309,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 24673,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1155883379096788992/9DHjnL6T_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155883379096788992/9DHjnL6T_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/717067922158411777/1472396584",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 75,
+		"favorite_count": 436,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Teachers ~ https://t.co/reSzmMfT0T"
+	},
+	{
 		"created_at": "Thu Jun 17 07:27:44 +0000 2021",
 		"id": 1405312404737638400,
 		"id_str": "1405426922306293762",
