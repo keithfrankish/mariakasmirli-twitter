96215592812a807dbc4347c12b101e5ae7200afb
Keith Frankish <k.frankish@gmail.com>
2021-08-15 03:11:46 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2263,
-	"created_at": "Mon Jul 26 00:11:44 +0000 2021",
+	"tweets": 2264,
+	"created_at": "Sun Aug 15 00:11:44 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Sat Aug 14 04:51:50 +0000 2021",
+		"id": 1426258895777972200,
+		"id_str": "1426406184219779073",
+		"full_text": "Supporting each other can help us better navigate the upcoming school year! https://t.co/Q5pmVjPloa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			75
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1426258891705311200,
+					"id_str": "1426258891705311235",
+					"indices": [
+						76,
+						99
+					],
+					"media_url": "http://pbs.twimg.com/media/E8sW58XWYAMOk8W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8sW58XWYAMOk8W.jpg",
+					"url": "https://t.co/Q5pmVjPloa",
+					"display_url": "pic.twitter.com/Q5pmVjPloa",
+					"expanded_url": "https://twitter.com/LRobbTeacher/status/1426258895777972224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 566,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 721,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1426258891705311200,
+					"id_str": "1426258891705311235",
+					"indices": [
+						76,
+						99
+					],
+					"media_url": "http://pbs.twimg.com/media/E8sW58XWYAMOk8W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8sW58XWYAMOk8W.jpg",
+					"url": "https://t.co/Q5pmVjPloa",
+					"display_url": "pic.twitter.com/Q5pmVjPloa",
+					"expanded_url": "https://twitter.com/LRobbTeacher/status/1426258895777972224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 566,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 721,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 430807468,
+			"id_str": "430807468",
+			"name": "Laura Robb",
+			"screen_name": "LRobbTeacher",
+			"location": "Winchester, VA 22601",
+			"description": "Author, teacher, consultant, keynote speaker with more than 40 years teaching experience. Interested in helping teachers reach every student. #TeamMakers",
+			"url": "https://t.co/N97w8dLp1Z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/N97w8dLp1Z",
+							"expanded_url": "http://www.LRobb.com",
+							"display_url": "LRobb.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20617,
+			"friends_count": 6631,
+			"listed_count": 182,
+			"created_at": "Wed Dec 07 15:56:26 +0000 2011",
+			"favourites_count": 64749,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65302,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1101122332079341569/guoSCQZU_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101122332079341569/guoSCQZU_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/430807468/1489613085",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 176,
+		"favorite_count": 636,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Supporting each other can help us better navigate the upcoming school year! https://t.co/Q5pmVjPloa"
+	}
+]
