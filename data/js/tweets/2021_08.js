44c9c3e3ca2269ae5185cfb08d650ce3ae4f6153
Keith Frankish <k.frankish@gmail.com>
2021-08-18 03:11:45 +0300

    Added 1 tweets

--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 17 06:21:11 +0000 2021",
+		"id": 1424705193577525200,
+		"id_str": "1427515834138865670",
+		"full_text": "My book, THE RIGHT TO SEX, is out in the UK next week. Here it is, pictured with my editor (and sternest critic). \n\nYou can preorder a copy from the @LRBbookshop here: https://t.co/oVkziFT6Bj. Also available at https://t.co/ii5EpMVNeH and the other usual places. https://t.co/7aUxJHwR8L",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LRBbookshop",
+					"name": "LRB Bookshop",
+					"id": 14849266,
+					"id_str": "14849266",
+					"indices": [
+						149,
+						161
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oVkziFT6Bj",
+					"expanded_url": "https://www.londonreviewbookshop.co.uk/stock/the-right-to-sex-amia-srinivasan",
+					"display_url": "londonreviewbookshop.co.uk/stock/the-righ…",
+					"indices": [
+						168,
+						191
+					]
+				},
+				{
+					"url": "https://t.co/ii5EpMVNeH",
+					"expanded_url": "http://bookshop.org",
+					"display_url": "bookshop.org",
+					"indices": [
+						211,
+						234
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1424705189530022000,
+					"id_str": "1424705189530021888",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/E8WR0kuXsAAde0B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8WR0kuXsAAde0B.jpg",
+					"url": "https://t.co/7aUxJHwR8L",
+					"display_url": "pic.twitter.com/7aUxJHwR8L",
+					"expanded_url": "https://twitter.com/amiasrinivasan/status/1424705193577525249/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1424705189530022000,
+					"id_str": "1424705189530021888",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/E8WR0kuXsAAde0B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8WR0kuXsAAde0B.jpg",
+					"url": "https://t.co/7aUxJHwR8L",
+					"display_url": "pic.twitter.com/7aUxJHwR8L",
+					"expanded_url": "https://twitter.com/amiasrinivasan/status/1424705193577525249/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14651591,
+			"id_str": "14651591",
+			"name": "Amia Srinivasan",
+			"screen_name": "amiasrinivasan",
+			"location": "London, Oxford",
+			"description": "Confirming that all ravens are black, one non-black non-raven at a time.",
+			"url": "https://t.co/HrFSbDm8DM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HrFSbDm8DM",
+							"expanded_url": "http://users.ox.ac.uk/~corp1468/Welcome.html",
+							"display_url": "users.ox.ac.uk/~corp1468/Welc…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23386,
+			"friends_count": 1266,
+			"listed_count": 187,
+			"created_at": "Sun May 04 20:32:13 +0000 2008",
+			"favourites_count": 2928,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 895,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/508986310346805248/5koi-1XP_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/508986310346805248/5koi-1XP_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14651591/1628586631",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 93,
+		"favorite_count": 854,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My book, THE RIGHT TO SEX, is out in the UK next week. Here it is, pictured with my editor (and sternest critic). \n\nYou can preorder a copy from the @LRBbookshop here: https://t.co/oVkziFT6Bj. Also available at https://t.co/ii5EpMVNeH and the other usual places. https://t.co/7aUxJHwR8L"
+	},
+	{
 		"created_at": "Sat Aug 14 04:51:50 +0000 2021",
 		"id": 1426258895777972200,
 		"id_str": "1426406184219779073",
